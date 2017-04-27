ff20c85121b176e967b9b291a6bac2ab2b2ef5d7
Zou Ming <zouming.zouming@huawei.com>
2017-04-27 08:05:01 +0000

    nvme-cli: Huawei plug-in.
    This patch adds support for Vendor unique commands for Huawei devices.
    Following commands are supported in current version of this extension:
    
    1.list  List addition info (such as namespace name and array name) for Huawei devices

---
+++ huawei-nvme.h
@@ -0,0 +1,17 @@
+#undef CMD_INC_FILE
+#define CMD_INC_FILE huawei-nvme
+
+#if !defined(HUAWEI_NVME) || defined(CMD_HEADER_MULTI_READ)
+#define HUAWEI_NVME
+
+#include "cmd.h"
+
+PLUGIN(NAME("huawei", "Huawei vendor specific extensions"),
+	COMMAND_LIST(
+		ENTRY("list", "List all Huawei NVMe devices and namespaces on machine", huawei_list)
+	)
+);
+
+#endif
+
+#include "define_cmd.h"
