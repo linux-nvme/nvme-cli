linux-nvme/nvme-cli

commit 662c1618d2cd16093168533b4f9f403538b51fc3

nvme-cli: add print for command specific info field of error log page
Add print line for Command Specific Information[39:32] in Error
Information Log Entry.

Signed-off-by: Minwoo Im <minwoo.im.dev@gmail.com>

nvme-print.c
@@ -947,6 +947,7 @@ void show_error_log(struct nvme_error_log_page *err_log, int entries, const char
 		printf("lba          : %#"PRIx64"\n",(uint64_t)le64_to_cpu(err_log[i].lba));
 		printf("nsid         : %#x\n", err_log[i].nsid);
 		printf("vs           : %d\n", err_log[i].vs);
+		printf("cs           : %#"PRIx64"\n", (uint64_t) err_log[i].cs);
 		printf(".................\n");
 	}
 }

nvme.h
@@ -48,7 +48,9 @@ struct nvme_error_log_page {
 	__u64	lba;
 	__u32	nsid;
 	__u8	vs;
-	__u8	resv[35];
+	__u8	resv[3];
+	__u64	cs;
+	__u8	resv2[24];
 };
 
 struct nvme_firmware_log_page {

commit 2cae38c63b3c0abc53fe9ab20bb8b3f84bf61cd9

nvme-cli: add command specific field to json output of error-log
Add command specific field to json output of error-log command.
This field was added in commit 662c1618 without considering json output
format.

Fixes: 662c1618("nvme-cli: add print for command specific info field of
error log page")
Signed-off-by: Minwoo Im <minwoo.im.dev@gmail.com>

nvme-print.c
@@ -1841,6 +1841,7 @@ void json_error_log(struct nvme_error_log_page *err_log, int entries, const char
 		json_object_add_value_int(error, "lba", err_log[i].lba);
 		json_object_add_value_int(error, "nsid", err_log[i].nsid);
 		json_object_add_value_int(error, "vs", err_log[i].vs);
+		json_object_add_value_uint(error, "cs", err_log[i].cs);
 
 		json_array_add_value_object(errors, error);
 	}