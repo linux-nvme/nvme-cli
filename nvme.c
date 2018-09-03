94eb4b3a6401790b9b2c0878d46fe6a982d9fdf0
Eyal Ben David <eyalbe@il.ibm.com>
2018-09-03 10:09:06 +0300

    Fix f28 (gcc 8.1) compilation

--- nvme.c
+++ nvme.c
@@ -1245,7 +1245,7 @@ static const char *subsys_dir = "/sys/class/nvme-subsystem/";
 
 static char *get_nvme_subsnqn(char *path)
 {
-	char sspath[319];
+	char sspath[FILENAME_MAX];
 	char *subsysnqn;
 	int fd;
 	int ret;
