d56e7e250105f8ed685fae79a7c7cf9187284cd0
yongxing <yongxing.sun@shannon-sys.com>
2019-06-10 22:56:33 +0800

    NVMe-Cli Shannon Plugin-add support for shannon systems.
        Hello,current NVMe-Cli tools hasn't supprot for shannon systems, we want to add it.
        May we add some specific commands?
        We add four NVMe commands for shannon temporarily,smart-log-add, get-feature-add, set-feature-add,
        and id-ctrl.
    
        Any problems, please contact me with:
    
        Email:yongxing.sun@shannon-sys.com

---
+++ plugins/shannon/shannon-nvme.h
@@ -0,0 +1,20 @@
+#undef CMD_INC_FILE
+#define CMD_INC_FILE plugins/shannon/shannon-nvme
+
+#if !defined(SHANNON_NVME) || defined(CMD_HEADER_MULTI_READ)
+#define SHANNON_NVME
+
+#include "cmd.h"
+
+PLUGIN(NAME("shannon", "Shannon vendor specific extensions"),
+	COMMAND_LIST(
+		ENTRY("smart-log-add", "Retrieve Shannon SMART Log, show it", get_additional_smart_log)
+		ENTRY("get-feature-add", "Get Shannon feature and show the resulting value", get_additional_feature)
+		ENTRY("set-feature-add", "Set a Shannon feature and show the resulting value", set_additional_feature)
+		ENTRY("id-ctrl", "Shannon NVMe Identify Controller", shannon_id_ctrl)
+	)
+);
+
+#endif
+
+#include "define_cmd.h"
