7bed61ad5d02d1c9bc3313f38ddca47e4cabdcda
Keith Busch <kbusch@kernel.org>
2019-11-07 04:02:42 +0900

    fix huawei filter to search all namespaces
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- plugins/huawei/huawei-nvme.c
+++ plugins/huawei/huawei-nvme.c
@@ -322,7 +322,7 @@ static int huawei_list(int argc, char **argv, struct command *command,
 	if (fmt != JSON && fmt != NORMAL)
 		return -EINVAL;
 
-	n = scandir(dev, &devices, scan_dev_filter, alphasort);
+	n = scandir(dev, &devices, scan_namespace_filter, alphasort);
 	if (n <= 0)
 		return n;
 
