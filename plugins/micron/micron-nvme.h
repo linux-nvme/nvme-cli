83a01ba113e3fc120a50b63c82f6cfddf6474898
Hanumanthu Hanok <hanumanthuh@micron.com>
2019-11-27 06:23:10 +0530

    Micron plugin updates

--- plugins/micron/micron-nvme.h
+++ plugins/micron/micron-nvme.h
@@ -9,6 +9,11 @@
 PLUGIN(NAME("micron", "Micron vendor specific extensions"),
 	COMMAND_LIST(
 		ENTRY("select-download", "Selective Firmware Download", micron_selective_download)
+                ENTRY("vs-temperature-stats", "Retrieve Micron temperature statistics ", micron_temp_stats)
+                ENTRY("vs-pcie-stats", "Retrieve Micron PCIe error stats", micron_pcie_stats)
+                ENTRY("clear-pcie-correctable-errors", "Clear correctable PCIe errors", micron_clear_pcie_correctable_errors)
+                ENTRY("vs-internal-log", "Retrieve Micron logs", micron_internal_logs)
+                ENTRY("vs-nand-stats", "Retrieve NAND Stats", micron_nand_stats)
 	)
 );
 
