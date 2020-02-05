bc5f35b4b8a5d1d1d794c03f0916754bc7ee4d74
Jeff Lien <jeff.lien@wdc.com>
2020-02-05 14:36:26 -0600

    Add namespace-resize wdc plugin command for SN720 drives.

--- plugins/wdc/wdc-nvme.h
+++ plugins/wdc/wdc-nvme.h
@@ -28,6 +28,7 @@ PLUGIN(NAME("wdc", "Western Digital vendor specific extensions"),
 		ENTRY("vs-telemetry-controller-option", "WDC Enable/Disable Controller Initiated Telemetry Log", wdc_vs_telemetry_controller_option)
 		ENTRY("vs-error-reason-identifier", "WDC Telemetry Reason Identifier", wdc_reason_identifier)
 		ENTRY("log-page-directory", "WDC Get Log Page Directory", wdc_log_page_directory)
+		ENTRY("namespace-resize", "WDC NamespaceDrive Resize", wdc_namespace_resize)
 	)
 );
 
