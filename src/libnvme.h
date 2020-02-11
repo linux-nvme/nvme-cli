7da106fc54cdb79f425c9112beff157409a749f2
Keith Busch <kbusch@kernel.org>
2020-02-10 17:38:36 -0800

    Add c++ inclusion support
    extern "C" the top level header file, and add a simple c++ compile test.
    
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- src/libnvme.h
+++ src/libnvme.h
@@ -1,11 +1,19 @@
 #ifndef _LIBNVME_H
 #define _LIBNVME_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "nvme/types.h"
 #include "nvme/ioctl.h"
 #include "nvme/fabrics.h"
 #include "nvme/filters.h"
 #include "nvme/tree.h"
 #include "nvme/util.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _LIBNVME_H */
