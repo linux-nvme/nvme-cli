f30e2b277cbe19376be885d723be738d5bc84ad9
Gregory C. Oakes <gregoryoakes@fastmail.com>
2020-03-12 12:36:56 -0500

    Fixed malformed json output from sanitize-log.

--- nvme-print.c
+++ nvme-print.c
@@ -3672,6 +3672,7 @@ void nvme_show_sanitize_log(struct nvme_sanitize_log_page *sanitize,
 		d_raw((unsigned char *)sanitize, sizeof(*sanitize));
 	else if (flags & JSON)
 		json_sanitize_log(sanitize, devname);
+        return;
 
 	printf("Sanitize Progress                      (SPROG) :  %u",
 	       le16_to_cpu(sanitize->progress));
