6555f936f3e07a6a87384c7752b8b231aa5c38b3
丁剑 <jian.ding@memblaze.com>
2020-05-12 11:49:09 +0800

    [memblaze] add select-download command
    1. This performs a selective firmware download;
    2. Please power cycle for changes to take effect, after the update successful.
    
    Signed-off-by: 丁剑 <jian.ding@memblaze.com>

--- plugins/memblaze/memblaze-nvme.h
+++ plugins/memblaze/memblaze-nvme.h
@@ -5,15 +5,23 @@
 #define MEMBLAZE_NVME
 
 #include "cmd.h"
+#include "common.h"
+
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 PLUGIN(NAME("memblaze", "Memblaze vendor specific extensions"),
 	COMMAND_LIST(
 		ENTRY("smart-log-add", "Retrieve Memblaze SMART Log, show it", get_additional_smart_log)
 		ENTRY("get-feature-add", "Get Memblaze feature and show the resulting value", get_additional_feature)
 		ENTRY("set-feature-add", "Set a Memblaze feature and show the resulting value", set_additional_feature)
+		ENTRY("select-download", "Selective Firmware Download", memblaze_selective_download)
 	)
 );
 
 #endif
 
 #include "define_cmd.h"
+
