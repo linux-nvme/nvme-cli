e15d9c4134ba38892a9531e59d742bbd7772c636
James Yang <james.yang@scaleflux.com>
2020-05-27 15:58:55 -0700

    Add ScaleFlux subcommands

--- plugins/scaleflux/sfx-nvme.h
+++ plugins/scaleflux/sfx-nvme.h
@@ -7,15 +7,15 @@
 #include "cmd.h"
 
 PLUGIN(NAME("sfx", "ScaleFlux vendor specific extensions"),
-    COMMAND_LIST(
-        ENTRY("smart-log-add", "Retrieve ScaleFlux SMART Log, show it", get_additional_smart_log)
-        ENTRY("lat-stats", "Retrieve ScaleFlux IO Latency Statistics log, show it", get_lat_stats_log)
-        ENTRY("get-bad-block", "Retrieve bad block table of block device, show it", sfx_get_bad_block)
-        ENTRY("query-cap", "Query current capacity info", query_cap_info)
-        ENTRY("change-cap", "Dynamic change capacity", change_cap)
-        ENTRY("set-feature", "Set a feature", sfx_set_feature)
-        ENTRY("get-feature", "get a feature", sfx_get_feature)
-    )
+	COMMAND_LIST(
+		ENTRY("smart-log-add", "Retrieve ScaleFlux SMART Log, show it", get_additional_smart_log)
+		ENTRY("lat-stats", "Retrieve ScaleFlux IO Latency Statistics log, show it", get_lat_stats_log)
+		ENTRY("get-bad-block", "Retrieve bad block table of block device, show it", sfx_get_bad_block)
+		ENTRY("query-cap", "Query current capacity info", query_cap_info)
+		ENTRY("change-cap", "Dynamic change capacity", change_cap)
+		ENTRY("set-feature", "Set a feature", sfx_set_feature)
+		ENTRY("get-feature", "get a feature", sfx_get_feature)
+	)
 );
 
 #endif
