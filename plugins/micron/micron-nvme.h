abfd028d41e5fc3d6c977d9c758a4f3354cb439f
Hanumanthu H <hanumanthuh@micron.com>
2020-07-30 22:28:26 +0530

    Micron plugin and documentation updates

--- plugins/micron/micron-nvme.h
+++ plugins/micron/micron-nvme.h
@@ -12,7 +12,16 @@ PLUGIN(NAME("micron", "Micron vendor specific extensions"),
 		ENTRY("vs-pcie-stats", "Retrieve Micron PCIe error stats", micron_pcie_stats)
 		ENTRY("clear-pcie-correctable-errors", "Clear correctable PCIe errors", micron_clear_pcie_correctable_errors)
 		ENTRY("vs-internal-log", "Retrieve Micron logs", micron_internal_logs)
+ 		ENTRY("vs-telemetry-controller-option", "Enable/Disable controller telemetry log generation", micron_telemetry_cntrl_option)
 		ENTRY("vs-nand-stats", "Retrieve NAND Stats", micron_nand_stats)
+		ENTRY("vs-drive-info", "Retrieve Drive information", micron_drive_info)
+		ENTRY("plugin-version", "Display plugin version info", micron_plugin_version)
+		ENTRY("log-page-directory", "Retrieve log page directory", micron_logpage_dir)
+		ENTRY("vs-fw-activate-history", "Display FW activation history", micron_fw_activation_history)
+		ENTRY("vs-error-reason-identifier", "Retrieve Error reason", micron_error_reason)
+		ENTRY("vs-smart-add-log", "Retrieve extended SMART data", micron_ext_smart_logs)
+		ENTRY("clear-fw-activate-history", "Clear FW activation history", micron_clr_fw_activation_history)
+		ENTRY("vs-smbus-option", "Enable/Disable SMBUS on the drive", micron_smbus_option)
 	)
 );
 
