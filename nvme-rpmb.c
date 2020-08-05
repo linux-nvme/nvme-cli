536158909545db4ee3fcbaa3fa3d98924df54313
Hanumanthu H <hanumanthuh@micron.com>
2020-08-05 12:16:37 +0530

    Fix copyright typo

--- nvme-rpmb.c
+++ nvme-rpmb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Micron Techologies Ltd. All rights reserved.
+ * Copyright (C) 2020 Micron Techology Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
