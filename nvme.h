b8c7fb009f072a84632388e3f8a42092476238f0
Keith Busch <kbusch@kernel.org>
2021-08-06 13:58:17 -0700

    Revert "Bug fix unsigned int and int should use 64bit api for json-c"
    This reverts commit d127537694e5953551d58acf8717ab1483c38ed3.

--- nvme.h
+++ nvme.h
@@ -30,9 +30,9 @@
 #define json_free_object(o) json_object_put(o)
 #define json_free_array(a) json_object_put(a)
 #define json_object_add_value_uint(o, k, v) \
-	json_object_object_add(o, k, json_object_new_uint64(v))
+	json_object_object_add(o, k, json_object_new_int(v))
 #define json_object_add_value_int(o, k, v) \
-	json_object_object_add(o, k, json_object_new_int64(v))
+	json_object_object_add(o, k, json_object_new_int(v))
 #define json_object_add_value_float(o, k, v) \
 	json_object_object_add(o, k, json_object_new_double(v))
 #define json_object_add_value_string(o, k, v) \
