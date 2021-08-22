b3fe787616cea27f4cdc3a1d3753c4f518a5c176
Gollu Appalanaidu <anaidu.gollu@samsung.com>
2021-08-22 23:25:03 +0530

    nvme: add support for fid supported and effects log(lid = 0x12)
    Signed-off-by: Gollu Appalanaidu <anaidu.gollu@samsung.com>

--- nvme-ioctl.h
+++ nvme-ioctl.h
@@ -122,6 +122,7 @@ int nvme_lba_status_log(int fd, void *lba_status, bool rae,
 		__u32 size);
 int nvme_resv_notif_log(int fd, struct nvme_resv_notif_log *resv);
 int nvme_boot_part_log(int fd, __u8 lsp, void *data, __u32 size);
+int nvme_fid_support_effects_log(int fd, struct nvme_fid_support_effects *fid_log);
 int nvme_feature(int fd, __u8 opcode, __u32 nsid, __u32 cdw10,
 		 __u32 cdw11, __u32 cdw12, __u32 cdw14, __u32 data_len, void *data,
 		 __u32 *result);
