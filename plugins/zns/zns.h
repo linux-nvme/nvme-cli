f78628a0040131b28b5dd28837f21dd452e61cc9
Andreas Hindborg <andreas.hindborg@wdc.com>
2021-09-13 13:13:53 +0000

    zns: Add list command
    Signed-off-by: Andreas Hindborg <andreas.hindborg@wdc.com>

--- plugins/zns/zns.h
+++ plugins/zns/zns.h
@@ -21,6 +21,7 @@ PLUGIN(NAME("zns", "Zoned Namespace Command Set"),
 		ENTRY("set-zone-desc", "Attaches zone descriptor extension data", set_zone_desc)
 		ENTRY("zone-append", "Writes data and metadata (if applicable), appended to the end of the requested zone", zone_append)
 		ENTRY("changed-zone-list", "Retrieves the changed zone list log", changed_zone_list)
+		ENTRY("list", "List ZNS enabled devices", list)
 	)
 );
 
