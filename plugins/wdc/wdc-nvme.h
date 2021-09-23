bb82ac047fb28003d1ef3c6d510dc92a038c9976
Jeff Lien <jeff.lien@wdc.com>
2021-09-23 12:23:16 -0500

    Add support for C3/Latency Monitor Log page parsing This support is for the WDC plugin
    Signed-off-by: Jeff Lien <jeff.lien@wdc.com>
    [dwagner: ported from nvme-cli-monolithic]
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- plugins/wdc/wdc-nvme.h
+++ plugins/wdc/wdc-nvme.h
@@ -35,6 +35,7 @@ PLUGIN(NAME("wdc", "Western Digital vendor specific extensions"),
 		ENTRY("capabilities", "WDC Device Capabilities", wdc_capabilities)
 		ENTRY("cloud-SSD-plugin-version", "WDC Cloud SSD Plugin Version", wdc_cloud_ssd_plugin_version)
 		ENTRY("vs-pcie-stats", "WDC VS PCIE Statistics", wdc_vs_pcie_stats)
+		ENTRY("get-latency-monitor-log", "WDC Get Latency Monitor Log Page", wdc_get_latency_monitor_log)
 	)
 );
 
