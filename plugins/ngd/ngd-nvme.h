c6a5e18ada4fcf0bf28fabcc7e9df9b813b70e6a
Hossein <31414094+Bobarshad@users.noreply.github.com>
2022-02-08 15:41:16 -0800

    Update ngd-nvme.h

--- plugins/ngd/ngd-nvme.h
+++ plugins/ngd/ngd-nvme.h
@@ -8,14 +8,7 @@
 
 PLUGIN(NAME("ngd", "NGDSysytems vendor specific extensions", NVME_VERSION),
 	COMMAND_LIST(
-		ENTRY("id-ctrl", "Send NVMe Identify Controller", id_ctrl)
 		ENTRY("internal-log", "Retrieve Ngd internal firmware log, save it", get_internal_log)
-		ENTRY("lat-stats", "Retrieve Ngd IO Latency Statistics log, show it", get_lat_stats_log)
-		ENTRY("set-bucket-thresholds", "Set Latency Stats Bucket Values, save it", set_lat_stats_thresholds)
-		ENTRY("lat-stats-tracking", "Enable and disable Latency Statistics logging.", enable_lat_stats_tracking)
-		ENTRY("market-name", "Retrieve Ngd Marketing Name log, show it", get_market_log)
-		ENTRY("smart-log-add", "Retrieve Ngd SMART Log, show it", get_additional_smart_log)
-		ENTRY("temp-stats", "Retrieve Ngd Temperature Statistics log, show it", get_temp_stats_log)
 	)
 );
 
