9bf17051829665d845207aa6212e43b44582c665
Daniel Wagner <dwagner@suse.de>
2022-02-22 17:45:58 +0100

    doc: Do not precomple HTML doc anymore
    The HTML part is not ready yet. Let's remove the precompiled HTML doc.
    
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- doc/update-docs.sh
+++ doc/update-docs.sh
@@ -5,13 +5,15 @@ DESTDIR=$(pwd)
 BUILDDIR="$(mktemp -d)"
 trap 'rm -rf -- $BUILDDIR' EXIT
 
-meson $BUILDDIR -Ddocs=all -Ddocs-build=true
+meson $BUILDDIR -Ddocs=man -Ddocs-build=true
 ninja -C $BUILDDIR
 
 rm -rf $DESTDIR/doc/man
-rm -rf $DESTDIR/doc/html
 mkdir $DESTDIR/doc/man
 
-cp -R $BUILDDIR/doc/html $DESTDIR/doc/
 find $BUILDDIR/doc -maxdepth 1 -name '*.2' -exec cp {} $DESTDIR/doc/man \;
+
+# The HTML doc is not ready yet
+# rm -rf $DESTDIR/doc/html
+# cp -R $BUILDDIR/doc/html $DESTDIR/doc/
 
