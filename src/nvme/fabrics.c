34b5d8e1a25fad61465188495f5b37ac84a41bcf
Tomas Bzatek <tbzatek@redhat.com>
2022-03-18 17:43:20 +0100

    fabrics: Add missing break in a switch
    No functional change, just a static analyzer warning.

--- src/nvme/fabrics.c
+++ src/nvme/fabrics.c
@@ -593,6 +593,7 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 			errno = EINVAL;
 			return NULL;
 		}
+		break;
 	case NVMF_TRTYPE_LOOP:
 		nvme_chomp(e->traddr, NVMF_TRADDR_SIZE);
 		traddr = strlen(e->traddr) ? e->traddr : NULL;
