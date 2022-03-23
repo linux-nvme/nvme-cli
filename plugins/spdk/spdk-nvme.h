acbe7336f52eb2793e337d87beb1aff6ea9162c3
Tomasz Zawadzki <tomasz.zawadzki@intel.com>
2022-03-23 13:41:40 +0100

    plugins/spdk: plugin to list SPDK controllers
    Since libnvme and nvme-cli(2.0) are continuing as separate projects,
    and libnvme use sysfs exclusively to gather information about NVMe
    devices, legacy paths that allow specifying a custom directory ('-d`)
    is no longer available.
    
    SPDK, as a userspace driver, does not create a sysfs entry but makes it
    possible to expose such devices via NVMe-cuse and since it behaves as
    character device, most commands should be intact, except
    listing devices and subsystems.
    
    This patch adds SPDK devices to the nvme_root topology tree, which
    allows for keeping functionality where both SPDK and kernel devices
    are printed.
    ```
            $nvme spdk list
    ```
    
    Signed-off-by: Sebastian Brzezinka <sebastian.brzezinka@intel.com>
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

---
+++ plugins/spdk/spdk-nvme.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#undef CMD_INC_FILE
+#define CMD_INC_FILE plugins/spdk/spdk-nvme
+
+#if !defined(SPDK_NVME) || defined(CMD_HEADER_MULTI_READ)
+#define SPDK_NVME
+
+#include "cmd.h"
+
+PLUGIN(NAME("spdk", "SPDK specific extensions", NVME_VERSION),
+	COMMAND_LIST(
+		ENTRY("list", "List all SPDK NVMe devices and namespaces on machine", spdk_list)
+	)
+);
+
+#endif
+
+#include "define_cmd.h"
