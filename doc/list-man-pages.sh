aed8d49e0877f17c647e372b1ecdf0929aa20eb0
Daniel Wagner <dwagner@suse.de>
2022-09-02 08:24:35 +0200

    docs: Relax regex in identifier match in list-man-pages.sh
    Function, struct and enum identifiers are allowed to have digits in
    their name, just not as first character. Relax the regex accordingly.
    
    Reported-by: Lee Duncan <lduncan@suse.com>
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- doc/list-man-pages.sh
+++ doc/list-man-pages.sh
@@ -3,14 +3,14 @@
 
 file=$1
 
-for func in $(sed -n 's/ \* \([a-z_]*\)() -.*/\1/p' $file); do
+for func in $(sed -n 's/ \* \([a-z_][a-z_0-9]*\)() -.*/\1/p' $file); do
 	echo ${func}
 done
 
-for struct in $(sed -n 's/ \* struct \([a-z_]*\) -.*/\1/p' $file); do
+for struct in $(sed -n 's/ \* struct \([a-z_][a-z_0-9]*\) -.*/\1/p' $file); do
 	echo ${struct}
 done
 
-for enum in $(sed -n 's/ \* enum \([a-z_]*\) -.*/\1/p' $file); do
+for enum in $(sed -n 's/ \* enum \([a-z_][a-z_0-9]*\) -.*/\1/p' $file); do
 	echo ${enum}
 done
