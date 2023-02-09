linux-nvme/nvme-cli

commit 1e5abd545622cbbd1cc566e53435900d5a091ddd

util: Update suffix_binary_parse API
Use the same API type for suffix_binary_parse as we have
for suffix_si_parse. While at it also introduce a unit test
for it.

Reviewed-by: Tokunori Ikegami <ikegami.t@gmail.com>
Signed-off-by: Daniel Wagner <dwagner@suse.de>

util/argconfig.c
@@ -293,8 +293,7 @@ int argconfig_parse(int argc, char *argv[], const char *program_desc,
 				goto out;
 			}
 		} else if (s->config_type == CFG_LONG_SUFFIX) {
-			*((uint64_t *)value_addr) = suffix_binary_parse(optarg);
-			if (errno) {
+			if (suffix_binary_parse(optarg, &endptr, (uint64_t*)&value_addr)) {
 				fprintf(stderr,
 					"Expected long suffixed integer argument for '%s' but got '%s'!\n",
 					long_opts[option_index].name, optarg);

util/suffix.c
@@ -167,14 +167,15 @@ static struct binary_suffix {
 	{30, "Gi"},
 	{20, "Mi"},
 	{10, "Ki"},
-	{0, ""}
 };
 
 const char *suffix_binary_get(long long *value)
 {
-	struct binary_suffix *s;
+	int i;
 
-	for (s = binary_suffixes; s->shift != 0; s++) {
+	for (i = 0; i < ARRAY_SIZE(binary_suffixes); i++) {
+		struct binary_suffix *s = &binary_suffixes[i];
+
 		if (llabs(*value) >= (1LL << s->shift)) {
 			*value =
 			    (*value + (1LL << (s->shift - 1))) / (1LL << s->shift);
@@ -187,9 +188,11 @@ const char *suffix_binary_get(long long *value)
 
 const char *suffix_dbinary_get(double *value)
 {
-	struct binary_suffix *s;
+	int i;
 
-	for (s = binary_suffixes; s->shift != 0; s++) {
+	for (i = 0; i < ARRAY_SIZE(binary_suffixes); i++) {
+		struct binary_suffix *s = &binary_suffixes[i];
+
 		if (fabs(*value) >= (1LL << s->shift)) {
 			*value = *value / (1LL << s->shift);
 			return s->suffix;
@@ -199,24 +202,35 @@ const char *suffix_dbinary_get(double *value)
 	return "";
 }
 
-uint64_t suffix_binary_parse(const char *value)
+int suffix_binary_parse(const char *str, char **endptr, uint64_t *val)
 {
-	char *suffix;
-	errno = 0;
-	uint64_t ret = strtoull(value, &suffix, 0);
-	if (errno)
-		return 0;
+	uint64_t ret;
+	int i;
 
-	struct binary_suffix *s;
-	for (s = binary_suffixes; s->shift != 0; s++) {
-		if (tolower(suffix[0]) == tolower(s->suffix[0])) {
+	ret = strtoull(str, endptr, 0);
+	if (str == *endptr ||
+	    ((ret == ULLONG_MAX) && errno == ERANGE))
+		return -EINVAL;
+
+	if (str == *endptr) {
+		*val = ret;
+		return 0;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(binary_suffixes); i++) {
+		struct binary_suffix *s = &binary_suffixes[i];
+
+		if (tolower((*endptr)[0]) == tolower(s->suffix[0]) &&
+		    (s->suffix[0] != '\0' &&
+		     (((*endptr)[0] != '\0' &&
+		       (*endptr)[1] != '\0' &&
+		       (*endptr)[2] == '\0') &&
+		      (tolower((*endptr)[1]) == tolower(s->suffix[1]))))) {
 			ret <<= s->shift;
-			return ret;
+			*val = ret;
+			return 0;
 		}
 	}
 
-	if (suffix[0] != '\0')
-		errno = EINVAL;
-
-	return ret;
+	return -EINVAL;
 }

unit/test-suffix-binary-parse.c
@@ -1,0 +1,68 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "../util/suffix.h"
+#include "../util/types.h"
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+static int test_rc;
+
+static void check_num(const char *val, __u64 exp, __u64 num)
+{
+	if (exp == num)
+		return;
+
+	printf("ERROR: printing {%s},  got '%llu', expected '%llu'\n",
+	       val, (unsigned long long)num, (unsigned long long)exp);
+
+	test_rc = 1;
+}
+
+struct tonum_test {
+	const char *val;
+	const uint64_t exp;
+	int ret;
+};
+
+static struct tonum_test tonum_tests[] = {
+	{ "1Ki", 1024, 0},
+	{ "34Gi", 36507222016, 0 },
+	{ "1234", 0, -EINVAL },
+	{ "34.9Ki", 0, -EINVAL},
+	{ "32Gii", 0, -EINVAL },
+};
+
+void tonum_test(struct tonum_test *test)
+{
+	char *endptr;
+	uint64_t num;
+	int ret;
+
+	ret = suffix_binary_parse(test->val, &endptr, &num);
+	if (ret != test->ret) {
+		printf("ERROR: converting {%s} failed\n", test->val);
+		test_rc = 1;
+		return;
+	}
+	if (ret)
+		return;
+
+	check_num(test->val, test->exp, num);
+}
+
+int main(void)
+{
+	unsigned int i;
+
+	test_rc = 0;
+
+	for (i = 0; i < ARRAY_SIZE(tonum_tests); i++)
+		tonum_test(&tonum_tests[i]);
+
+	return test_rc ? EXIT_FAILURE : EXIT_SUCCESS;
+}

unit/meson.build
@@ -18,6 +18,15 @@ test_suffix_si_parse = executable(
 
 test('suffix_si_parse', test_suffix_si_parse)
 
+test_suffix_binary_parse = executable(
+    'test-suffix-binary-parse',
+    ['test-suffix-binary-parse.c', '../util/suffix.c'],
+    include_directories: [incdir, '..'],
+    dependencies: [libnvme_dep],
+)
+
+test('suffix_binary_parse', test_suffix_binary_parse)
+
 test_uint128_si = executable(
     'test-uint128-si',
     ['test-uint128-si.c', '../util/types.c', '../util/suffix.c'],

util/suffix.h
@@ -40,6 +40,6 @@ int suffix_si_parse(const char *str, char **endptr, uint64_t *val);
 const char *suffix_si_get_ld(long double *value);
 const char *suffix_binary_get(long long *value);
 const char *suffix_dbinary_get(double *value);
-uint64_t suffix_binary_parse(const char *value);
+int suffix_binary_parse(const char *str, char **endptr, uint64_t *val);
 
 #endif

commit 0e8318170216d4e0ee5d5289a376e0744634f227

util: Numbers without any suffix are also valid
suffix_binary_parse() is used to parse most of the numeric command
lines. As it turns out the main use case to provide just numbers without
any suffix. Fix the regression recently introduced when we made the
parser also to check two characters suffixes.

Fixes: 1e5abd545622 ("util: Update suffix_binary_parse API")
Reported-by: Steven Seungcheol Lee <sc108.lee@samsung.com>
Signed-off-by: Daniel Wagner <dwagner@suse.de>

util/argconfig.c
@@ -293,7 +293,7 @@ int argconfig_parse(int argc, char *argv[], const char *program_desc,
 				goto out;
 			}
 		} else if (s->config_type == CFG_LONG_SUFFIX) {
-			if (suffix_binary_parse(optarg, &endptr, (uint64_t*)&value_addr)) {
+			if (suffix_binary_parse(optarg, &endptr, (uint64_t*)value_addr)) {
 				fprintf(stderr,
 					"Expected long suffixed integer argument for '%s' but got '%s'!\n",
 					long_opts[option_index].name, optarg);

util/suffix.c
@@ -217,6 +217,12 @@ int suffix_binary_parse(const char *str, char **endptr, uint64_t *val)
 		return 0;
 	}
 
+	/* simple number, no decimal point, no suffix */
+	if ((*endptr)[0] == '\0') {
+		*val = ret;
+		return 0;
+	}
+
 	for (i = 0; i < ARRAY_SIZE(binary_suffixes); i++) {
 		struct binary_suffix *s = &binary_suffixes[i];
 

unit/test-suffix-binary-parse.c
@@ -30,9 +30,9 @@ struct tonum_test {
 };
 
 static struct tonum_test tonum_tests[] = {
+	{ "1234", 1234, 0 },
 	{ "1Ki", 1024, 0},
 	{ "34Gi", 36507222016, 0 },
-	{ "1234", 0, -EINVAL },
 	{ "34.9Ki", 0, -EINVAL},
 	{ "32Gii", 0, -EINVAL },
 };