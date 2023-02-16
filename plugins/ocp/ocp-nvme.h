fa41184caa51939a3b7d096f244faea76923ace1
Dedow, Karl <karl.dedow@solidigm.com>
2023-02-16 13:07:25 -0800

    plugins/ocp: Fix whitespace and style issues
    As reported by a code linter results in OCP plugin
    
    [dwagner:
     - lot's of manual cleanup
     - fixed non white space changes reported by checktool
     - verified result with 'git diff --word-diff'
    ]
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- plugins/ocp/ocp-nvme.h
+++ plugins/ocp/ocp-nvme.h
@@ -3,7 +3,7 @@
  *
  * Authors: Arthur Shau <arthurshau@fb.com>,
  *          Wei Zhang <wzhang@fb.com>,
- *   	    Venkat Ramesh <venkatraghavan@fb.com>
+ *          Venkat Ramesh <venkatraghavan@fb.com>
  */
 #undef CMD_INC_FILE
 #define CMD_INC_FILE plugins/ocp/ocp-nvme
@@ -16,10 +16,8 @@
 PLUGIN(NAME("ocp", "OCP cloud SSD extensions", NVME_VERSION),
 	COMMAND_LIST(
 		ENTRY("smart-add-log", "Retrieve extended SMART Information", ocp_smart_add_log)
-		ENTRY("latency-monitor-log", "Get Latency Monitor Log Page",
-		      ocp_latency_monitor_log)
-		ENTRY("clear-fw-activate-history", "Clear firmware update history log",
-		      clear_fw_update_history)
+		ENTRY("latency-monitor-log", "Get Latency Monitor Log Page", ocp_latency_monitor_log)
+		ENTRY("clear-fw-activate-history", "Clear firmware update history log", clear_fw_update_history)
 	)
 );
 
