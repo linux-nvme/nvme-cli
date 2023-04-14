34c197e0ac2d57734ab775f00b2d0758aca7b82f
John Meneghini <jmeneghi@redhat.com>
2023-04-14 10:53:58 -0400

    nbft: make lookup_ctrl function public
    To prepare for the addition of nbft functionality fixup the fabrics
    declarations.
    
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>

--- fabrics.h
+++ fabrics.h
@@ -2,6 +2,16 @@
 #ifndef _FABRICS_H
 #define _FABRICS_H
 
+struct tr_config {
+	const char *subsysnqn;
+	const char *transport;
+	const char *traddr;
+	const char *host_traddr;
+	const char *host_iface;
+	const char *trsvcid;
+};
+
+extern nvme_ctrl_t lookup_ctrl(nvme_root_t r, struct tr_config *trcfg);
 extern int nvmf_discover(const char *desc, int argc, char **argv, bool connect);
 extern int nvmf_connect(const char *desc, int argc, char **argv);
 extern int nvmf_disconnect(const char *desc, int argc, char **argv);
