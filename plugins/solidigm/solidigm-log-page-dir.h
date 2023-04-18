d0b84da2b9cd20b65d0f5c47ede2d9a69f7bc447
kdedow <karl.dedow@solidigmtechnology.com>
2023-04-18 10:30:39 -0700

    solidigm: Add supported log pages log feature to Solidigm plugin

---
+++ plugins/solidigm/solidigm-log-page-dir.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2023 Solidigm.
+ *
+ * Authors: karl.dedow@solidigm.com
+ */
+
+#ifndef SOLIDIGM_LOG_PAGE_DIRECTORY_H
+#define SOLIDIGM_LOG_PAGE_DIRECTORY_H
+
+struct command;
+struct plugin;
+
+int solidigm_get_log_page_directory_log(int argc, char **argv, struct command *cmd,
+					struct plugin *plugin);
+
+#endif
