ce1d4bb4492bf720c39a689593973c08d1a7500c
jaya.ganthan <jaya.ganthan@samsung.com>
2023-04-28 08:40:56 +0530

    plugin/ocp: Added implementation for 0xC1 log page
    Reviewed-by: Karthik Balan <karthik.b82@samsung.com>
    Reviewed-by: Arunpandian J <arun.j@samsung.com>
    Signed-off-by: Jayakanthan Rajendran <jaya.ganthan@samsung.com>

--- plugins/ocp/ocp-nvme.h
+++ plugins/ocp/ocp-nvme.h
@@ -24,6 +24,7 @@ PLUGIN(NAME("ocp", "OCP cloud SSD extensions", NVME_VERSION),
 		ENTRY("clear-pcie-correctable-error-counters", "Clear PCIe correctable error counters", clear_pcie_corectable_error_counters)
 		ENTRY("vs-fw-activate-history", "Get firmware activation history log", fw_activation_history_log)
 		ENTRY("unsupported-reqs-log", "Get Unsupported Requirements Log Page", ocp_unsupported_requirements_log)
+		ENTRY("error-recovery-log", "Retrieve Error Recovery Log Page", ocp_error_recovery_log)
 	)
 );
 
