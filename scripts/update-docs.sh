332b1a4e070a62a78a0b8130917e2ce28f4cb35f
Daniel Wagner <dwagner@suse.de>
2023-06-23 09:20:49 +0200

    scripts: Update update-docs.sh scripts
    libnvme is not downloaded automatically anymore. Thus use
    --force-fallback-for to fetch it when updating the documentation.
    
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -6,8 +6,12 @@ cd "$(git rev-parse --show-toplevel)" || exit 1
 BUILDDIR="$(mktemp -d)"
 trap 'rm -rf -- $BUILDDIR' EXIT
 
-meson $BUILDDIR -Ddocs=all -Ddocs-build=true
-ninja -C $BUILDDIR
-find $BUILDDIR/Documentation -maxdepth 1 \
+meson setup                        \
+     -Ddocs=all                    \
+     -Ddocs-build=true             \
+     --force-fallback-for=libnvme  \
+     "${BUILDDIR}"
+meson compile -C "${BUILDDIR}"
+find "${BUILDDIR}/Documentation" -maxdepth 1 \
      \( -name '*.1' -o -name '*.html' \) \
      -exec cp {} Documentation/ \;
