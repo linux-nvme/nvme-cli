4416fa2ef58ed35aa0bf858c82f7f08530ad4c9b
Daniel Wagner <dwagner@suse.de>
2023-06-23 10:52:44 +0200

    scripts: Call update doc script from top level dir
    Make sure that the script runs from the lop level dir.
    
    While at it also properly quote variables to make shellcheck happy.
    
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -1,38 +1,46 @@
 #!/bin/sh
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-DESTDIR=$(pwd)
+cd "$(git rev-parse --show-toplevel)" || exit 1
 
 # build man docs
 BUILDDIR="$(mktemp -d)"
-echo $BUILDDIR
-trap 'rm -rf -- $BUILDDIR' EXIT
+echo "${BUILDDIR}"
+trap 'rm -rf -- ${BUILDDIR}' EXIT
 
-meson setup $BUILDDIR -Ddocs=man -Ddocs-build=true
-meson compile -C $BUILDDIR
+meson setup 		\
+    -Ddocs=man		\
+    -Ddocs-build=true	\
+    "${BUILDDIR}"
+meson compile 		\
+    -C "${BUILDDIR}"
 
-rm -rf $DESTDIR/doc/man
-mkdir $DESTDIR/doc/man
+rm -rf doc/man
+mkdir doc/man
 
-find $BUILDDIR/doc -maxdepth 1 -name '*.2' -exec cp {} $DESTDIR/doc/man \;
+find "${BUILDDIR}/doc" -maxdepth 1 -name '*.2' -exec cp {} doc/man \;
 
 # build ReST docs
-rm -rf -- $BUILDDIR
+rm -rf -- "${BUILDDIR}"
 BUILDDIR="$(mktemp -d)"
-echo $BUILDDIR
-trap 'rm -rf -- $BUILDDIR' EXIT
+echo "${BUILDDIR}"
+trap 'rm -rf -- ${BUILDDIR}' EXIT
 
-meson setup $BUILDDIR -Ddocs=rst -Ddocs-build=true
-meson compile -C $BUILDDIR
+meson setup 		\
+    -Ddocs=rst 		\
+    -Ddocs-build=true	\
+    "${BUILDDIR}"
+meson compile 		\
+    -C "${BUILDDIR}"
 
-rm -rf $DESTDIR/doc/rst/*.rst
-mkdir -p $DESTDIR/doc/rst
+rm -rf doc/rst/*.rst
+mkdir -p doc/rst
 
-find $BUILDDIR/doc/rst -maxdepth 1 -name '*.rst' -exec cp {} $DESTDIR/doc/rst \;
+find "${BUILDDIR}/doc/rst" -maxdepth 1 -name '*.rst' -exec cp {} doc/rst \;
 
-cp $BUILDDIR/doc/conf.py $DESTDIR/doc
-cp $BUILDDIR/doc/index.rst $DESTDIR/doc
-cp $BUILDDIR/doc/config-schema.json $DESTDIR/doc
+cp "${BUILDDIR}/doc/conf.py" doc
+cp "${BUILDDIR}/doc/index.rst" doc
+cp "${BUILDDIR}/doc/config-schema.json" doc
 
 # build html docs
 # The HTML doc is not ready yet
