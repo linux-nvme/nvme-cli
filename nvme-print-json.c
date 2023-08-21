a97b7ebaa5ee8e6e3a63a093211f7897289ed26a
Tokunori Ikegami <ikegami.t@gmail.com>
2023-08-22 00:52:17 +0900

    nvme-print: Fix endurance_log whitespace changes
    Signed-off-by: Tokunori Ikegami <ikegami.t@gmail.com>

--- nvme-print-json.c
+++ nvme-print-json.c
@@ -454,51 +454,36 @@ void json_changed_ns_list_log(struct nvme_ns_list *log,
 	json_free_object(root);
 }
 
-static void json_endurance_log(struct nvme_endurance_group_log *endurance_group,
-			       __u16 group_id, const char *devname)
+static void json_endurance_log(struct nvme_endurance_group_log *endurance_group, __u16 group_id,
+			       const char *devname)
 {
 	struct json_object *root;
-
-	nvme_uint128_t endurance_estimate =
-		le128_to_cpu(endurance_group->endurance_estimate);
-	nvme_uint128_t data_units_read =
-		le128_to_cpu(endurance_group->data_units_read);
-	nvme_uint128_t data_units_written =
-		le128_to_cpu(endurance_group->data_units_written);
-	nvme_uint128_t media_units_written =
-		le128_to_cpu(endurance_group->media_units_written);
-	nvme_uint128_t host_read_cmds =
-		le128_to_cpu(endurance_group->host_read_cmds);
-	nvme_uint128_t host_write_cmds =
-		le128_to_cpu(endurance_group->host_write_cmds);
+	nvme_uint128_t endurance_estimate = le128_to_cpu(endurance_group->endurance_estimate);
+	nvme_uint128_t data_units_read = le128_to_cpu(endurance_group->data_units_read);
+	nvme_uint128_t data_units_written = le128_to_cpu(endurance_group->data_units_written);
+	nvme_uint128_t media_units_written = le128_to_cpu(endurance_group->media_units_written);
+	nvme_uint128_t host_read_cmds = le128_to_cpu(endurance_group->host_read_cmds);
+	nvme_uint128_t host_write_cmds = le128_to_cpu(endurance_group->host_write_cmds);
 	nvme_uint128_t media_data_integrity_err =
-		le128_to_cpu(endurance_group->media_data_integrity_err);
+	    le128_to_cpu(endurance_group->media_data_integrity_err);
 	nvme_uint128_t num_err_info_log_entries =
-		le128_to_cpu(endurance_group->num_err_info_log_entries);
+	    le128_to_cpu(endurance_group->num_err_info_log_entries);
 
 	root = json_create_object();
 
-	json_object_add_value_int(root, "critical_warning",
-		endurance_group->critical_warning);
-	json_object_add_value_int(root, "avl_spare",
-		endurance_group->avl_spare);
+	json_object_add_value_int(root, "critical_warning", endurance_group->critical_warning);
+	json_object_add_value_int(root, "avl_spare", endurance_group->avl_spare);
 	json_object_add_value_int(root, "avl_spare_threshold",
-		endurance_group->avl_spare_threshold);
-	json_object_add_value_int(root, "percent_used",
-		endurance_group->percent_used);
-	json_object_add_value_uint128(root, "endurance_estimate",
-		endurance_estimate);
+				  endurance_group->avl_spare_threshold);
+	json_object_add_value_int(root, "percent_used", endurance_group->percent_used);
+	json_object_add_value_uint128(root, "endurance_estimate", endurance_estimate);
 	json_object_add_value_uint128(root, "data_units_read", data_units_read);
-	json_object_add_value_uint128(root, "data_units_written",
-		data_units_written);
-	json_object_add_value_uint128(root, "media_units_written",
-		media_units_written);
+	json_object_add_value_uint128(root, "data_units_written", data_units_written);
+	json_object_add_value_uint128(root, "media_units_written", media_units_written);
 	json_object_add_value_uint128(root, "host_read_cmds", host_read_cmds);
 	json_object_add_value_uint128(root, "host_write_cmds", host_write_cmds);
-	json_object_add_value_uint128(root, "media_data_integrity_err",
-		media_data_integrity_err);
-	json_object_add_value_uint128(root, "num_err_info_log_entries",
-		num_err_info_log_entries);
+	json_object_add_value_uint128(root, "media_data_integrity_err", media_data_integrity_err);
+	json_object_add_value_uint128(root, "num_err_info_log_entries", num_err_info_log_entries);
 
 	json_print_object(root, NULL);
 	printf("\n");
