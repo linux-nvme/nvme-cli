374d6b4c9f98d56686f6afb504108e24b683e346
Leonardo da Cunha <leonardo.da.cunha@solidigm.com>
2024-04-18 16:50:30 -0700

    plugins/ocp: Split ocp_get_uuid_index() into find and get functions.
    Enable reuse of OCP UUID discovery logic from other plugins.
    Enable single call to nvme_identify_uuid() when dealing with multiple UUIDs
    
    Signed-off-by: Leonardo da Cunha <leonardo.da.cunha@solidigm.com>

--- plugins/ocp/ocp-utils.h
+++ plugins/ocp/ocp-utils.h
@@ -1,18 +1,32 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Copyright (c) 2022 Solidigm.
+ * Copyright (c) 2022-2024 Solidigm.
  *
  * Author: leonardo.da.cunha@solidigm.com
  */
 
 #include "nvme.h"
 
+/*
+ * UUID assigned for OCP.
+ */
+extern const unsigned char ocp_uuid[NVME_UUID_LEN];
+
 /**
  * ocp_get_uuid_index() - Get OCP UUID index
  * @dev:	nvme device
  * @index:	integer pointer to here to save the index
- * @result:	The command completion result from CQE dword0
  *
- * Return: Zero if nvme device has UUID list log page, or result of get uuid list otherwise.
+ * Return: Zero if nvme device has UUID list log page, or positive result of get uuid list
+ *         or negative POSIX error code otherwise.
  */
 int ocp_get_uuid_index(struct nvme_dev *dev, int *index);
+
+/**
+ * ocp_find_uuid_index() - Find OCP UUID index in UUID list
+ * @uuid_list:	uuid_list retrieved from Identify UUID List (CNS 0x17)
+ * @index:	integer pointer to here to save the index
+ *
+ * Return: Zero if nvme device has UUID list log page, Negative POSIX error code otherwise.
+ */
+int ocp_find_uuid_index(struct nvme_id_uuid_list *uuid_list, int *index);
