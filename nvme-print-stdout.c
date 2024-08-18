dd3f59014f41ead9454d1cb3c066c7167d565abd
Martin George <marting@netapp.com>
2024-08-18 22:24:18 +0530

    nvme-print: update subsys verbose outputs
    The current list-subsys & show-topology simple outputs are a
    little crowded. Also the respective verbose options to both
    these commands are a no-op at the moment. So move the iopolicy
    and subsystype details to the respective verbose outputs instead
    so that the simple outputs are restricted to displaying key subsys
    details like subsys name & NQN alone.
    
    Signed-off-by: Martin George <marting@netapp.com>

--- nvme-print-stdout.c
+++ nvme-print-stdout.c
@@ -1071,10 +1071,13 @@ static void stdout_subsys_config(nvme_subsystem_t s)
 	       nvme_subsystem_get_nqn(s));
 	printf("%*s   hostnqn=%s\n", len, " ",
 	       nvme_host_get_hostnqn(nvme_subsystem_get_host(s)));
-	printf("%*s   iopolicy=%s\n", len, " ",
-	       nvme_subsystem_get_iopolicy(s));
-	printf("%*s   type=%s\n", len, " ",
-	       nvme_subsystem_get_type(s));
+
+	if (stdout_print_ops.flags & VERBOSE) {
+		printf("%*s   iopolicy=%s\n", len, " ",
+			nvme_subsystem_get_iopolicy(s));
+		printf("%*s   type=%s\n", len, " ",
+			nvme_subsystem_get_type(s));
+	}
 }
 
 static void stdout_subsystem(nvme_root_t r, bool show_ana)
