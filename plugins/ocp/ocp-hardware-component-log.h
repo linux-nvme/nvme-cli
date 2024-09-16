d47fd54106631230324937f41c9b2fbdec528b24
Tokunori Ikegami <ikegami.t@gmail.com>
2024-09-16 20:59:30 +0900

    ocp: split ocp-hwcomp log
    This is to reduce the ocp-nvme.c code size.
    
    Signed-off-by: Tokunori Ikegami <ikegami.t@gmail.com>

---
+++ plugins/ocp/ocp-hardware-component-log.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2024
+ */
+#include "cmd.h"
+
+#ifndef OCP_HARDWARE_COMPONENT_LOG_H
+#define OCP_HARDWARE_COMPONENT_LOG_H
+
+int ocp_hwcomp_log(int argc, char **argv, struct command *cmd, struct plugin *plugin);
+
+#endif /* OCP_HARDWARE_COMPONENT_LOG_H */
