0e0f67b0abaedea0b47575e8a6095e7870f7a59d
Daniel Wagner <dwagner@suse.de>
2024-10-09 13:33:20 +0200

    test: make config-diff more flexible to use
    The config-diff script is expecting a sysfs tar file besides an input
    and a output file. Let's make the sysfs tar file optional so we can use
    this config diff script more flexible.
    
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- test/config/config-diff.sh
+++ test/config/config-diff.sh
@@ -1,24 +1,48 @@
 #!/bin/bash -e
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-BUILD_DIR=$1
-CONFIG_DUMP=$2
-SYSDIR_INPUT=$3
-CONFIG_JSON=$4
-EXPECTED_OUTPUT=$5
+positional_args=()
+sysfs_tar=""
+config_json=""
 
-ACTUAL_OUTPUT="${BUILD_DIR}"/$(basename "${EXPECTED_OUTPUT}")
+while [[ $# -gt 0 ]]; do
+    case $1 in
+	--sysfs-tar)
+	    sysfs_tar=$2
+	    shift 1
+	    ;;
+	--config-json)
+	    config_json=$2
+	    shift 1
+	    ;;
+    *)
+        positional_args+=("$1")
+        shift
+        ;;
+    esac
+done
 
-TEST_NAME="$(basename -s .tar.xz $SYSDIR_INPUT)"
-TEST_DIR="$BUILD_DIR/$TEST_NAME"
+set -- "${positional_args[@]}"
 
-rm -rf "${TEST_DIR}"
-mkdir "${TEST_DIR}"
-tar -x -f "${SYSDIR_INPUT}" -C "${TEST_DIR}"
+test_binary="$1"
+build_dir="$2"
+expected_output="$3"
 
-LIBNVME_SYSFS_PATH="$TEST_DIR" \
+sysfs_path=""
+if [[ -n "${sysfs_tar}" ]]; then
+   test_name="$(basename -s .tar.xz ${sysfs_tar})"
+   sysfs_path="${build_dir}/${test_name}"
+
+   rm -rf "${sysfs_path}"
+   mkdir "${sysfs_path}"
+   tar -x -f "${sysfs_tar}" -C "${sysfs_path}"
+fi
+
+output="${build_dir}"/$(basename "${expected_output}")
+
+LIBNVME_SYSFS_PATH="${sysfs_path}" \
 LIBNVME_HOSTNQN=nqn.2014-08.org.nvmexpress:uuid:ce4fee3e-c02c-11ee-8442-830d068a36c6 \
 LIBNVME_HOSTID=ce4fee3e-c02c-11ee-8442-830d068a36c6 \
-"${CONFIG_DUMP}" "${CONFIG_JSON}" > "${ACTUAL_OUTPUT}" || echo "test failed"
+"${test_binary}" "${config_json}" > "${output}" || echo "test failed"
 
-diff -u "${EXPECTED_OUTPUT}" "${ACTUAL_OUTPUT}"
+diff -u "${expected_output}" "${output}"
