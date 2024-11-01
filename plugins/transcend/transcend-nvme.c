1f9aa256df4d7e0e8f3f2d0e6d4d593875f9f3c9
JerryWang <jerry_wang@transcend-info.com>
2024-11-01 16:53:10 +0800

    Correct coding style and naming rules
    Signed-off-by: WBJisMyName <snowsummersp@gmail.com>

--- plugins/transcend/transcend-nvme.c
+++ plugins/transcend/transcend-nvme.c
@@ -5,19 +5,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <inttypes.h>
+#include <string.h>
+#include <time.h>
 
 #include "nvme.h"
 #include "libnvme.h"
 #include "plugin.h"
-#include <string.h>
-#include <time.h>
+#include "transcend-nvme.h"
 
 #define CREATE_CMD
 
-#define PLPRecordPath "PLPRec.txt"
-#define PLPDataExpiredTime 20
-
-#include "transcend-nvme.h"
+#define PLP_RECORD_PATH "PLPRec.txt"
+#define PLP_DATA_EXPIRED_TIME 20
 
 static const __u32 OP_BAD_BLOCK = 0xc2;
 static const __u32 DW10_BAD_BLOCK = 0x400;
@@ -30,342 +29,327 @@ static const __u32 DW12_HEALTH = 0xb3;
 static const __u32 OP_ID = 0x06;
 static const __u32 DW10_ID = 0x01;
 
-static const int iDis = 20;
-static const double fullValue = 170;
+static const int i_dis = 20;
+static const double full_value = 170;
 
-enum PLPErrorCode {
-	PLP_ERROR_NO_MATCH = -1,
-	PLP_ERROR_DATA_EXPIRED = -2
+enum plp_error_code {
+    PLP_ERROR_NO_MATCH = -1,
+    PLP_ERROR_DATA_EXPIRED = -2
 };
 
 const char *string_list[] = {
-	"UTE210T",
-	"MTE712P",
-	"MTE560P",
-	"MTE662P",
-	"MTE662P-I",
-	"MTS970P",
-	"MTS952P",
-	"MTS952P-I",
-	"MTS570P",
-	"MTS400P",
-	"SSD910T",
-	"SSD470P",
-	"SSD470P-I",
-	"SSD452P",
-	"SSD452P-I",
-	"SSD420P",
-	"MSA470P",
-	"MSA452P"};
-const int list_size = ARRAY_SIZE(string_list) / sizeof(string_list[0]);
+    "UTE210T",   "MTE712P",   "MTE560P",   "MTE662P", "MTE662P-I", "MTS970P",
+    "MTS952P",   "MTS952P-I", "MTS570P",   "MTS400P", "SSD910T",   "SSD470P",
+    "SSD470P-I", "SSD452P",   "SSD452P-I", "SSD420P", "MSA470P",   "MSA452P"
+};
 
-static int getPLPHealth(int argc, char **argv, struct command *cmd, struct plugin *plugin);
-static int readUsefulPLPValue(const char *device);
-static void recordPLPValue(const char *device, int value, bool isReplace);
+const int list_size = ARRAY_SIZE(string_list);
 
-static int getHealthValue(int argc, char **argv, struct command *cmd, struct plugin *plugin)
+static int get_plp_health(int argc, char **argv, struct command *cmd, struct plugin *plugin);
+static int read_useful_plp_value(const char *device);
+static void record_plp_value(const char *device, int value, bool is_replace);
+
+static int get_health_value(int argc, char **argv, struct command *cmd, struct plugin *plugin)
 {
-	struct nvme_smart_log smart_log;
-	char *desc = "Get nvme health percentage.";
-	int percent_used = 0;
-	int healthvalue = 0;
-	struct nvme_dev *dev;
-	int result;
+    struct nvme_smart_log smart_log;
+    char *desc = "Get nvme health percentage.";
+    int percent_used = 0;
+    int health_value = 0;
+    struct nvme_dev *dev;
+    int result;
 
-	OPT_ARGS(opts) = {
-		OPT_END()
-	};
+    OPT_ARGS(opts) = {
+        OPT_END()
+    };
 
-	result = parse_and_open(&dev, argc, argv, desc, opts);
-	if (result) {
-		printf("\nDevice not found\n");
-		return PLP_ERROR_NO_MATCH;
-	}
-	result = nvme_get_log_smart(dev_fd(dev), 0xffffffff, false, &smart_log);
-	if (!result) {
-		printf("Transcend NVME heath value: ");
-		percent_used = smart_log.percent_used;
+    result = parse_and_open(&dev, argc, argv, desc, opts);
+    if (result) {
+        printf("\nDevice not found\n");
+        return PLP_ERROR_NO_MATCH;
+    }
+    result = nvme_get_log_smart(dev_fd(dev), 0xffffffff, false, &smart_log);
+    if (!result) {
+        printf("Transcend NVME health value: ");
+        percent_used = smart_log.percent_used;
 
-		if (percent_used > 100 || percent_used < 0) {
-			printf("0%%\n");
-		} else {
-			healthvalue = 100 - percent_used;
-			printf("%d%%\n", healthvalue);
-		}
-	}
-	dev_close(dev);
-	return result;
+        if (percent_used > 100 || percent_used < 0) {
+            printf("0%%\n");
+        } else {
+            health_value = 100 - percent_used;
+            printf("%d%%\n", health_value);
+        }
+    }
+    dev_close(dev);
+    return result;
 }
 
-static int getBadblock(int argc, char **argv, struct command *cmd, struct plugin *plugin)
+static int get_bad_block(int argc, char **argv, struct command *cmd, struct plugin *plugin)
 {
+    char *desc = "Get nvme bad block number.";
+    struct nvme_dev *dev;
+    int result;
 
-	char *desc = "Get nvme bad block number.";
-	struct nvme_dev *dev;
-	int result;
+    OPT_ARGS(opts) = {
+        OPT_END()
+    };
 
-	OPT_ARGS(opts) = {
-		OPT_END()
-	};
+    result = parse_and_open(&dev, argc, argv, desc, opts);
+    if (result) {
+        printf("\nDevice not found\n");
+        return -1;
+    }
+    unsigned char data[1] = {0};
+    struct nvme_passthru_cmd nvme_cmd;
 
-	result = parse_and_open(&dev, argc, argv, desc, opts);
-	if (result) {
-		printf("\nDevice not found\n");
-		return -1;
-	}
-	unsigned char data[1] = {0};
-	struct nvme_passthru_cmd nvmecmd;
-
-	memset(&nvmecmd, 0, sizeof(nvmecmd));
-	nvmecmd.opcode = OP_BAD_BLOCK;
-	nvmecmd.cdw10 = DW10_BAD_BLOCK;
-	nvmecmd.cdw12 = DW12_BAD_BLOCK;
-	nvmecmd.addr = (__u64)(uintptr_t)data;
-	nvmecmd.data_len = 0x1;
-	result = nvme_submit_admin_passthru(dev_fd(dev), &nvmecmd, NULL);
-	if (!result) {
-		int badblock = data[0];
-		printf("Transcend NVME badblock count: %d\n", badblock);
-	}
-	dev_close(dev);
-	return result;
+    memset(&nvme_cmd, 0, sizeof(nvme_cmd));
+    nvme_cmd.opcode = OP_BAD_BLOCK;
+    nvme_cmd.cdw10 = DW10_BAD_BLOCK;
+    nvme_cmd.cdw12 = DW12_BAD_BLOCK;
+    nvme_cmd.addr = (__u64)(uintptr_t)data;
+    nvme_cmd.data_len = 0x1;
+    result = nvme_submit_admin_passthru(dev_fd(dev), &nvme_cmd, NULL);
+    if (!result) {
+        int bad_block = data[0];
+        printf("Transcend NVME badblock count: %d\n", bad_block);
+    }
+    dev_close(dev);
+    return result;
 }
 
-const char *format_char_array(char *str, int strsize, unsigned char *chr, int chrsize)
+const char *format_char_array(char *str, int str_size, unsigned char *chr, int chr_size)
 {
-	int b = 0;
-	int n = 0;
-	int i;
+    int b = 0;
+    int n = 0;
+    int i;
 
-	while (b < chrsize && chr[b] == ' ')
-		b++;
+    while (b < chr_size && chr[b] == ' ')
+        b++;
 
-	while (b + n < chrsize && chr[b + n])
-		n++;
+    while (b + n < chr_size && chr[b + n])
+        n++;
 
-	while (n > 0 && chr[b + n - 1] == ' ')
-		n--;
+    while (n > 0 && chr[b + n - 1] == ' ')
+        n--;
 
-	if (n >= strsize)
-		n = strsize - 1;
+    if (n >= str_size)
+        n = str_size - 1;
 
-	for (i = 0; i < n; i++) {
-		char c = chr[b + i];
-		str[i] = (' ' <= c && c <= '~' ? c : '?');
-	}
+    for (i = 0; i < n; i++) {
+        char c = chr[b + i];
+        str[i] = (' ' <= c && c <= '~' ? c : '?');
+    }
 
-	str[n] = 0;
-	return str;
+    str[n] = 0;
+    return str;
 }
 
 int contains_string(const char *str)
 {
-	for (int i = 0; i < list_size; i++)
-	{
-		if (strstr(str, string_list[i]) != NULL)
-		{
-			return 1;
-		}
-	}
-	return 0;
+    int i;
+
+    for (i = 0; i < list_size; i++) {
+        if (strstr(str, string_list[i]) != NULL) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
-static int getPLPHealth(int argc, char **argv, struct command *cmd, struct plugin *plugin)
+static int get_plp_health(int argc, char **argv, struct command *cmd, struct plugin *plugin)
 {
-	char *desc = "Get nvme PLP Health.";
-	struct nvme_dev *dev;
-	int result;
-	int txtPLPHealth = -1;
-	int plpHealth_percentage = -1;
-	OPT_ARGS(opts) = {
-		OPT_END()
-	};
+    char *desc = "Get nvme PLP Health.";
+    struct nvme_dev *dev;
+    int result;
+    int txt_plp_health = -1;
+    int plp_health_percentage = -1;
+    OPT_ARGS(opts) = {
+        OPT_END()
+    };
 
-	result = parse_and_open(&dev, argc, argv, desc, opts);
-	if (result) {
-		printf("\nDevice not found\n");
-		return -1;
-	}
+    result = parse_and_open(&dev, argc, argv, desc, opts);
+    if (result) {
+        printf("\nDevice not found\n");
+        return -1;
+    }
 
-	unsigned char dataID[4096];
-	struct nvme_passthru_cmd nvmecmdID;
+    unsigned char data_id[4096];
+    struct nvme_passthru_cmd nvme_cmd_id;
 
-	memset(&nvmecmdID, 0, sizeof(nvmecmdID));
-	nvmecmdID.opcode = OP_ID;
-	nvmecmdID.cdw10 = DW10_ID;
-	nvmecmdID.addr = (__u64)(uintptr_t)dataID;
-	nvmecmdID.data_len = 4096;
-	result = nvme_submit_admin_passthru(dev_fd(dev), &nvmecmdID, NULL);
-	if (result) {
-		printf("\nThis device is not support.\n");
-		return -1;
-	} else {
-		char modelName[40];
-		const char *model_str_byte;
-		model_str_byte = format_char_array((char *)modelName, sizeof(modelName), dataID, sizeof(dataID));
-		if (!contains_string(model_str_byte)) {
-			printf("\nThis device is not support.\n");
-			return -1;
-		}
-	}
+    memset(&nvme_cmd_id, 0, sizeof(nvme_cmd_id));
+    nvme_cmd_id.opcode = OP_ID;
+    nvme_cmd_id.cdw10 = DW10_ID;
+    nvme_cmd_id.addr = (__u64)(uintptr_t)data_id;
+    nvme_cmd_id.data_len = 4096;
+    result = nvme_submit_admin_passthru(dev_fd(dev), &nvme_cmd_id, NULL);
+    if (result) {
+        printf("\nThis device is not supported.\n");
+        dev_close(dev);
+        return -1;
+    }
+    char model_name[40];
+    const char *model_str_byte;
+    model_str_byte = format_char_array((char *)model_name, sizeof(model_name), data_id, sizeof(data_id));
+    if (!contains_string(model_str_byte)) {
+        printf("\nThis device is not supported.\n");
+        dev_close(dev);
+        return -1;
+    }
 
-	txtPLPHealth = readUsefulPLPValue(dev->name);
-	if (txtPLPHealth >= 0) {
-		plpHealth_percentage = txtPLPHealth;
-		printf("Capacitor health for PLP: %d%%\n", plpHealth_percentage);
-	} else {
-		unsigned char data[512];
-		struct nvme_passthru_cmd nvmecmd;
+    txt_plp_health = read_useful_plp_value(dev->name);
+    if (txt_plp_health >= 0) {
+        plp_health_percentage = txt_plp_health;
+        printf("Capacitor health for PLP: %d%%\n", plp_health_percentage);
+    } else {
+        unsigned char data[512];
+        struct nvme_passthru_cmd nvme_cmd;
 
-		memset(&nvmecmd, 0, sizeof(nvmecmd));
-		nvmecmd.opcode = OP_HEALTH;
-		nvmecmd.cdw10 = DW10_HEALTH;
-		nvmecmd.cdw12 = DW12_HEALTH;
-		nvmecmd.addr = (__u64)(uintptr_t)data;
-		nvmecmd.data_len = 512;
-		result = nvme_submit_admin_passthru(dev_fd(dev), &nvmecmd, NULL);
-		if (result) {
-			printf("\nGet PLP Health Fail.\n");
-			return PLP_ERROR_NO_MATCH;
-		} else {
-			int tDis = (int)(data[4 + 3] << 24) + (int)(data[4 + 2] << 16) + (int)(data[4 + 1] << 8) + (int)(data[4]);
-			int vDis1 = (int)(data[0]);
-			int vDis2 = (int)(data[1]);
-			if (vDis1 != 0 || vDis2 != 0) {
-				int normalHealth = 0;
+        memset(&nvme_cmd, 0, sizeof(nvme_cmd));
+        nvme_cmd.opcode = OP_HEALTH;
+        nvme_cmd.cdw10 = DW10_HEALTH;
+        nvme_cmd.cdw12 = DW12_HEALTH;
+        nvme_cmd.addr = (__u64)(uintptr_t)data;
+        nvme_cmd.data_len = 512;
+        result = nvme_submit_admin_passthru(dev_fd(dev), &nvme_cmd, NULL);
+        if (result) {
+            printf("\nGet PLP Health Fail.\n");
+            dev_close(dev);
+            return PLP_ERROR_NO_MATCH;
+        } else {
+            int t_dis = (int)(data[4 + 3] << 24) + (int)(data[4 + 2] << 16) + (int)(data[4 + 1] << 8) + (int)(data[4]);
+            int v_dis1 = (int)(data[0]);
+            int v_dis2 = (int)(data[1]);
+            if (v_dis1 != 0 || v_dis2 != 0) {
+                int normal_health = 0;
 
-				if (vDis1 - vDis2 != 0) {
-					normalHealth = (iDis * tDis) / (vDis1 - vDis2);
-				} else {
-					normalHealth = 0;
-				}
-				if (normalHealth >= 0) {
-					if (fullValue - normalHealth >= 0) {
-						plpHealth_percentage = (normalHealth / fullValue) * 100;
-					} else {
-						plpHealth_percentage = 100;
-					}
-				} else {
-					plpHealth_percentage = 0;
-				}
-				printf("Capacitor health for PLP: %d%%\n", plpHealth_percentage);
-			}
-		}
-	}
-	if (plpHealth_percentage >= 0) {
-		if (txtPLPHealth == PLP_ERROR_DATA_EXPIRED) {
-			recordPLPValue(dev->name, plpHealth_percentage, true);
-		}
-		else {
-			recordPLPValue(dev->name, plpHealth_percentage, false);
-		}
-	}
-	dev_close(dev);
-	return result;
+                if (v_dis1 - v_dis2 != 0) {
+                    normal_health = (i_dis * t_dis) / (v_dis1 - v_dis2);
+                } else {
+                    normal_health = 0;
+                }
+                if (normal_health >= 0) {
+                    if (full_value - normal_health >= 0) {
+                        plp_health_percentage = (normal_health / full_value) * 100;
+                    } else {
+                        plp_health_percentage = 100;
+                    }
+                } else {
+                    plp_health_percentage = 0;
+                }
+                printf("Capacitor health for PLP: %d%%\n", plp_health_percentage);
+            }
+        }
+    }
+    if (plp_health_percentage >= 0) {
+        if (txt_plp_health == PLP_ERROR_DATA_EXPIRED) {
+            record_plp_value(dev->name, plp_health_percentage, true);
+        } else {
+            record_plp_value(dev->name, plp_health_percentage, false);
+        }
+    }
+    dev_close(dev);
+    return result;
 }
 
-int readUsefulPLPValue(const char *device)
+int read_useful_plp_value(const char *device)
 {
-	FILE *file;
-	char logFilePath[256];
-	char str[256];
-	char matchedStr[256] = "";
-	int ret = -1;
+    FILE *file;
+    char log_file_path[256];
+    char str[256];
+    char matched_str[256] = "";
+    int ret = -1;
 
-	snprintf(logFilePath, sizeof(logFilePath), "%s", PLPRecordPath);
-	file = fopen(logFilePath, "r");
-	if (!file) {
-		return PLP_ERROR_NO_MATCH;
-	}
+    snprintf(log_file_path, sizeof(log_file_path), "%s", PLP_RECORD_PATH);
+    file = fopen(log_file_path, "r");
+    if (!file) {
+        return PLP_ERROR_NO_MATCH;
+    }
 
-	while (fgets(str, sizeof(str), file))
-	{
-		if (strncmp(str, device, strlen(device)) == 0) {
-			strcpy(matchedStr, str);
-			break;
-		}
-	}
+    while (fgets(str, sizeof(str), file)) {
+        if (strncmp(str, device, strlen(device)) == 0) {
+            strcpy(matched_str, str);
+            break;
+        }
+    }
 
-	fclose(file);
+    fclose(file);
 
-	if (matchedStr[0] == '\0') {
-		return PLP_ERROR_NO_MATCH;
-	}
+    if (matched_str[0] == '\0') {
+        return PLP_ERROR_NO_MATCH;
+    }
 
-	char *token;
-	token = strtok(matchedStr, "#");
-	token = strtok(NULL, "#");
-	struct tm tm;
-	memset(&tm, 0, sizeof(tm));
-	strptime(token, "%a %b %d %H:%M:%S %Y", &tm);
-	time_t t = mktime(&tm);
+    char *token;
+    token = strtok(matched_str, "#");
+    token = strtok(NULL, "#");
+    struct tm tm;
+    memset(&tm, 0, sizeof(tm));
+    strptime(token, "%a %b %d %H:%M:%S %Y", &tm);
+    time_t t = mktime(&tm);
 
-	time_t t_current = time(NULL);
-	int timeDiff = difftime(t_current, t);
+    time_t t_current = time(NULL);
+    int time_diff = difftime(t_current, t);
 
-	if (timeDiff <= PLPDataExpiredTime) {
-		token = strtok(NULL, "#");
-		ret = atoi(token);
-		return ret;
-	} else {
-		return PLP_ERROR_DATA_EXPIRED;
-	}
+    if (time_diff <= PLP_DATA_EXPIRED_TIME) {
+        token = strtok(NULL, "#");
+        ret = atoi(token);
+        return ret;
+    } else {
+        return PLP_ERROR_DATA_EXPIRED;
+    }
 }
 
-void recordPLPValue(const char *device, int value, bool isReplace)
+void record_plp_value(const char *device, int value, bool is_replace)
 {
-	char logFilePath[256];
-	strncpy(logFilePath, PLPRecordPath, sizeof(logFilePath) - 1);
-	logFilePath[sizeof(logFilePath) - 1] = '\0';
-	char tempFilePath[] = "temp.txt";
+    char log_file_path[256];
+    strncpy(log_file_path, PLP_RECORD_PATH, sizeof(log_file_path) - 1);
+    log_file_path[sizeof(log_file_path) - 1] = '\0';
+    char temp_file_path[] = "temp.txt";
 
-	time_t ct = time(0);
-	char *timeStr = ctime(&ct);
-	if (timeStr == NULL) {
-		perror("Error getting current time");
-		return;
-	}
-	timeStr[strcspn(timeStr, "\n")] = '\0';
+    time_t ct = time(0);
+    char *time_str = ctime(&ct);
+    if (time_str == NULL) {
+        perror("Error getting current time");
+        return;
+    }
+    time_str[strcspn(time_str, "\n")] = '\0';
 
-	char line[256];
-	sprintf(line, "%s#%s#%d", device, timeStr, value);
+    char line[256];
+    snprintf(line, sizeof(line), "%s#%s#%d", device, time_str, value);
 
-	if (isReplace) {
-		FILE *filein = fopen(logFilePath, "r");
-		FILE *fileout = fopen(tempFilePath, "w");
-		if (filein == NULL || fileout == NULL) {
-			perror("Error opening file");
-			if (filein != NULL)
-				fclose(filein);
-			if (fileout != NULL)
-				fclose(fileout);
-			return;
-		}
+    if (is_replace) {
+        FILE *file_in = fopen(log_file_path, "r");
+        FILE *file_out = fopen(temp_file_path, "w");
+        if (file_in == NULL || file_out == NULL) {
+            perror("Error opening file");
+            if (file_in != NULL)
+                fclose(file_in);
+            if (file_out != NULL)
+                fclose(file_out);
+            return;
+        }
 
-		char str[256];
-		while (fgets(str, sizeof(str), filein))
-		{
-			if (strncmp(str, device, strlen(device)) == 0) {
-				fprintf(fileout, "%s\n", line);
-			} else {
-				fprintf(fileout, "%s", str);
-			}
-		}
-		fclose(filein);
-		fclose(fileout);
+        char str[256];
+        while (fgets(str, sizeof(str), file_in)) {
+            if (strncmp(str, device, strlen(device)) == 0) {
+                fprintf(file_out, "%s\n", line);
+            } else {
+                fprintf(file_out, "%s", str);
+            }
+        }
+        fclose(file_in);
+        fclose(file_out);
 
-		if (remove(logFilePath) == 0) {
-			rename(tempFilePath, logFilePath);
-		} else {
-			remove(tempFilePath);
-		}
-	} else {
-		FILE *out = fopen(logFilePath, "a");
-		if (out == NULL) {
-			perror("Error opening file");
-			return;
-		}
-		fprintf(out, "%s\n", line);
-		fclose(out);
-	}
+        if (remove(log_file_path) == 0) {
+            rename(temp_file_path, log_file_path);
+        } else {
+            remove(temp_file_path);
+        }
+    } else {
+        FILE *out = fopen(log_file_path, "a");
+        if (out == NULL) {
+            perror("Error opening file");
+            return;
+        }
+        fprintf(out, "%s\n", line);
+        fclose(out);
+    }
 }
--- plugins/transcend/transcend-nvme.h
+++ plugins/transcend/transcend-nvme.h
@@ -4,18 +4,20 @@
 
 #if !defined(TRANSCEND_NVME) || defined(CMD_HEADER_MULTI_READ)
 #define TRANSCEND_NVME
+
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 #include "cmd.h"
  
 
 PLUGIN(NAME("transcend", "Transcend vendor specific extensions", NVME_VERSION),
     COMMAND_LIST(
-			ENTRY("healthvalue", "NVME health percentage", getHealthValue)
-			ENTRY("badblock", "Get NVME bad block number", getBadblock)
-			ENTRY("plphealthvalue", "Get NVME PLP Health.", getPLPHealth)
+        ENTRY("healthvalue", "NVME health percentage", get_health_value)
+        ENTRY("badblock", "Get NVME bad block number", get_bad_block)
+        ENTRY("plphealthvalue", "Get NVME PLP Health.", get_plp_health)
     )
 );
 
-#endif
+#endif /* TRANSCEND_NVME */
 
 #include "define_cmd.h"
