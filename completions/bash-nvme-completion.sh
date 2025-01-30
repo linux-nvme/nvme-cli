82938193a02ed50c462b141fac6037def7341cee
Tokunori Ikegami <ikegami.t@gmail.com>
2025-01-30 23:40:38 +0900

    completions: add rotational-media-info-log command
    Both bash and zsh completions updated for the command.
    
    Signed-off-by: Tokunori Ikegami <ikegami.t@gmail.com>

--- completions/bash-nvme-completion.sh
+++ completions/bash-nvme-completion.sh
@@ -482,6 +482,10 @@ nvme_list_opts () {
 		"mgmt-addr-list-log")
 		opts+=" --verbose -v --output-format= -o --timeout= -t"
 			;;
+		"rotational-media-info-log")
+		opts+=" --endg-id= -e --verbose -v --output-format= -o \
+			--timeout= -t"
+			;;
 		"version")
 		opts+=$NO_OPTS
 			;;
@@ -1672,7 +1676,8 @@ _nvme_subcmds () {
 		rpmb boot-part-log fid-support-effects-log \
 		supported-log-pages lockdown media-unit-stat-log \
 		supported-cap-config-log dim show-topology list-endgrp \
-		nvme-mi-recv nvme-mi-send get-reg set-reg mgmt-addr-list-log"
+		nvme-mi-recv nvme-mi-send get-reg set-reg mgmt-addr-list-log \
+		rotational-media-info-log"
 
 	# Add plugins:
 	for plugin in "${!_plugin_subcmds[@]}"; do
