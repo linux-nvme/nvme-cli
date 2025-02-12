c57e9d2c0c071aa7c24e7f2daa3c871445a44ee9
Francis Pravin <francis.p@samsung.com>
2025-02-12 17:38:30 +0530

    types: add HDISNS field of Host Behavior Support feature
    Add the Host Dispersed Namespace Support (HDISNS) field of Host Behavior
    Support feature.
    NVM Express Base Specification 2.1
    
    Signed-off-by: Francis Pravin <francis.p@samsung.com>

--- src/nvme/types.h
+++ src/nvme/types.h
@@ -6126,15 +6126,15 @@ struct nvme_plm_config {
  * @acre:	Advanced Command Retry Enable
  * @etdas:	Extended Telemetry Data Area 4 Supported
  * @lbafee:	LBA Format Extension Enable
- * @rsvd3:	Reserved
- * @cdfe:       Copy Descriptor Formats Enable
+ * @hdisns:	Host Dispersed Namespace Support
+ * @cdfe:	Copy Descriptor Formats Enable
  * @rsvd6:	Reserved
  */
 struct nvme_feat_host_behavior {
 	__u8 acre;
 	__u8 etdas;
 	__u8 lbafee;
-	__u8 rsvd3;
+	__u8 hdisns;
 	__le16 cdfe;
 	__u8 rsvd6[506];
 };
