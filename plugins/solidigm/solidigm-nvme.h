46a8bc26ee53d2bc3d5326a8f0bd5c428a81f270
Leonardo da Cunha <leonardo.da.cunha@solidigm.com>
2025-02-27 14:44:34 -0800

    plugins/solidigm: Updated SMART PLL Lock Loss counters field
    Broke down additional SMART PLL Lock Loss field into separate counters.
    
    Signed-off-by: Leonardo da Cunha <leonardo.da.cunha@solidigm.com>

--- plugins/solidigm/solidigm-nvme.h
+++ plugins/solidigm/solidigm-nvme.h
@@ -13,7 +13,7 @@
 
 #include "cmd.h"
 
-#define SOLIDIGM_PLUGIN_VERSION "1.10"
+#define SOLIDIGM_PLUGIN_VERSION "1.11"
 
 PLUGIN(NAME("solidigm", "Solidigm vendor specific extensions", SOLIDIGM_PLUGIN_VERSION),
 	COMMAND_LIST(
