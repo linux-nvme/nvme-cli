ebf0750e04314fc5aaacb3772395b6716e8c2763
Nilay Shroff <nilay@linux.ibm.com>
2025-09-04 23:26:50 +0530

    nvme: add common APIs for printing tabular format output
    Some nvme-cli commands, such as nvme list and nvme list -v, support output
    in tabular format. Currently, the table output is not well aligned because
    column widths are fixed at print time, regardless of the length of the data
    in each column. This often results in uneven and hard-to-read output.For
    any new CLI command that requires tabular output, developers must manually
    specify the column width and row value width, which is both error-prone and
    inconsistent.
    
    This patch introduces a set of common table APIs that:
    - Automatically calculate column widths based on the content
    - Maintain proper alignment regardless of value length
    - Simplify adding tabular output support to new and existing commands
    
    The new APIs are:
    1. table_init() — Allocate a table instance.
    2. table_add_columns() — Add column definitions (struct table_column),
       including name and alignment (LEFT, RIGHT, CENTERED).
    3. table_add_columns_filter() - Same as table_add_columns() but also
       provide a filter function callback which could be then used by the
       caller for filtering any column.
    3. table_get_row_id() — Reserve a row index for inserting values.
    4. table_add_row() — Add a row to the table.
    5. table_print() — Print the table with auto-calculated widths.
    6. table_free() — Free resources allocated for the table.
    
    For adding values, the following setter APIs are provided, each
    supporting alignment types (LEFT, RIGHT, or CENTERED):
    - table_set_value_str()
    - table_set_value_int()
    - table_set_value_unsigned()
    - table_set_value_long()
    - table_set_value_unsigned_long()
    
    Usage steps:
    1. Call table_init() to create a table handle.
    2. Define an array of struct table_column specifying column names and
       alignment, then call table_add_columns() or if you want to filter
       column then table_add_columns_filter().
    3. Obtain a row ID using table_get_row_id() and set values using the
       appropriate setter table APIs : table_set_value_*() function.
    4. Add the completed row using table_add_row().
    5. Repeat steps 3–4 for each additional row.
    5. Call table_print() to display the table.
    6. Call table_free() to release table resources.
    
    With these APIs, developers no longer need to pre-calculate column or
    row widths. The output is consistently aligned and easy to read.
    
    Suggested-by: Daniel Wagner <dwagner@suse.de>
    Signed-off-by: Nilay Shroff <nilay@linux.ibm.com>
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

---
+++ util/table.h
@@ -0,0 +1,149 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _TABLE_H_
+#define _TABLE_H_
+
+#include <stdbool.h>
+
+enum fmt_type {
+	FMT_STRING,
+	FMT_INT,
+	FMT_UNSIGNED,
+	FMT_LONG,
+	FMT_UNSIGNED_LONG,
+};
+
+enum alignment {
+	RIGHT,
+	LEFT,
+	CENTERED
+};
+
+struct value {
+	union {
+		char *s;
+		int i;
+		unsigned int u;
+		long ld;
+		unsigned long lu;
+	};
+	enum alignment align;
+	enum fmt_type type;
+};
+
+struct table_row {
+	struct value *val;
+};
+
+struct table_column {
+	char *name;
+	enum alignment align;
+	int width;		/* auto populated */
+};
+
+struct table {
+	struct table_column *columns;
+	int num_columns;
+	struct table_row *rows;
+	int num_rows;
+};
+
+static inline int table_set_value_str(struct table *t, int col, int row,
+		const char *str, enum alignment align)
+{
+	struct table_row *r;
+	struct value *v;
+	char *s;
+
+	if (col >= t->num_columns || row >= t->num_rows)
+		return -EINVAL;
+
+	s = strdup(str);
+	if (!s)
+		return -ENOMEM;
+
+	r = &t->rows[row];
+	v = &r->val[col];
+	v->s = s;
+	v->align = align;
+	v->type = FMT_STRING;
+
+	return 0;
+}
+
+static inline int table_set_value_int(struct table *t, int col, int row,
+		int i, enum alignment align)
+{
+	struct table_row *r;
+	struct value *v;
+
+	if (col >= t->num_columns || row >= t->num_rows)
+		return -EINVAL;
+
+	r = &t->rows[row];
+	v = &r->val[col];
+	v->i = i;
+	v->align = align;
+	v->type = FMT_INT;
+
+	return 0;
+}
+
+static inline int table_set_value_unsigned(struct table *t, int col, int row,
+		int u, enum alignment align)
+{
+	struct table_row *r;
+	struct value *v;
+
+	if (col >= t->num_columns || row >= t->num_rows)
+		return -EINVAL;
+
+	r = &t->rows[row];
+	v = &r->val[col];
+	v->u = u;
+	v->align = align;
+	v->type = FMT_UNSIGNED;
+
+	return 0;
+}
+
+static inline int table_set_value_long(struct table *t, int col, int row,
+		long ld, enum alignment align)
+{
+	struct table_row *r;
+	struct value *v;
+
+	if (col >= t->num_columns || row >= t->num_rows)
+		return -EINVAL;
+
+	r = &t->rows[row];
+	v = &r->val[col];
+	v->ld = ld;
+	v->align = align;
+	v->type = FMT_LONG;
+
+	return 0;
+}
+
+static inline void table_set_value_unsigned_long(struct table *t, int col,
+		int row, long lu, enum alignment align)
+{
+	struct table_row *r = &t->rows[row];
+	struct value *v = &r->val[col];
+
+	v->lu = lu;
+	v->align = align;
+	v->type = FMT_UNSIGNED_LONG;
+}
+
+struct table *table_init(void);
+int table_add_columns(struct table *t, struct table_column *c, int num_columns);
+int table_add_columns_filter(struct table *t, struct table_column *c,
+			int num_columns,
+			bool (*filter)(const char *name, void *arg),
+			void *arg);
+int table_get_row_id(struct table *t);
+void table_add_row(struct table *t, int row);
+void table_print(struct table *t);
+void table_free(struct table *t);
+
+#endif /* _TABLE_H_ */
