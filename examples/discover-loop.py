89c132dc13e84f72cfd9b03bcc20f70641f7fe64
Hannes Reinecke <hare@suse.de>
2025-09-05 12:18:44 +0200

    examples/discover-loop.py: rework to use 'with' statement
    Now that we have resource tracking we can use the 'with' statement
    for discovery and clean up connections and discovery controllers
    on exit.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- examples/discover-loop.py
+++ examples/discover-loop.py
@@ -16,6 +16,33 @@ def disc_supp_str(dlp_supp_opts):
     }
     return [txt for msk, txt in d.items() if dlp_supp_opts & msk]
 
+def discover(host, ctrl):
+    try:
+        ctrl.connect(host)
+    except Exception as e:
+        print(f'Failed to connect: {e}')
+        return
+
+    print(f'{ctrl.name} connected to subsys {ctrl.subsystem}')
+
+    slp = ctrl.supported_log_pages()
+    try:
+        dlp_supp_opts = slp[nvme.NVME_LOG_LID_DISCOVER] >> 16
+    except (TypeError, IndexError):
+        dlp_supp_opts = 0
+
+    print(f"LID {nvme.NVME_LOG_LID_DISCOVER}h (Discovery), supports: {disc_supp_str(dlp_supp_opts)}")
+
+    try:
+        lsp = nvme.NVMF_LOG_DISC_LSP_PLEO if dlp_supp_opts & nvme.NVMF_LOG_DISC_LID_PLEOS else 0
+        disc_log = ctrl.discover(lsp=lsp)
+    except Exception as e:
+        print(f'Failed to discover: {e}')
+        return
+
+    for dlpe in disc_log:
+        print(f'log entry {dlpe["portid"]}: {dlpe["subtype"]} {dlpe["subnqn"]}')
+
 root = nvme.root()
 host = nvme.host(root)
 
@@ -24,38 +51,8 @@ transport = 'tcp'
 traddr = '127.0.0.1'
 trsvcid = '4420'
 
-ctrl = nvme.ctrl(root, subsysnqn=subsysnqn, transport=transport, traddr=traddr, trsvcid=trsvcid)
-
-try:
-    ctrl.connect(host)
-except Exception as e:
-    sys.exit(f'Failed to connect: {e}')
-
-print(f'{ctrl.name} connected to subsys {ctrl.subsystem}')
-
-slp = ctrl.supported_log_pages()
-
-try:
-    dlp_supp_opts = slp[nvme.NVME_LOG_LID_DISCOVER] >> 16
-except (TypeError, IndexError):
-    dlp_supp_opts = 0
-
-print(f"LID {nvme.NVME_LOG_LID_DISCOVER}h (Discovery), supports: {disc_supp_str(dlp_supp_opts)}")
-
-try:
-    lsp = nvme.NVMF_LOG_DISC_LSP_PLEO if dlp_supp_opts & nvme.NVMF_LOG_DISC_LID_PLEOS else 0
-    disc_log = ctrl.discover(lsp=lsp)
-except Exception as e:
-    print(f'Failed to discover: {e}')
-    disc_log = []
-
-for dlpe in disc_log:
-    print(f'log entry {dlpe["portid"]}: {dlpe["subtype"]} {dlpe["subnqn"]}')
-
-try:
-    ctrl.disconnect()
-except Exception as e:
-    sys.exit(f'Failed to disconnect: {e}')
+with nvme.ctrl(root, subsysnqn=subsysnqn, transport=transport, traddr=traddr, trsvcid=trsvcid) as ctrl:
+    discover(host, ctrl)
 
 for s in host.subsystems():
     for c in s.controllers():
