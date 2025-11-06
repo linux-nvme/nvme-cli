linux-nvme/libnvme

src: introduce transport handle abstraction
Abstract the file descriptor handle to a more abstract type so it's
possible to use different 'backends' to send/receive commands, e.g.
via MI instead direct.

Signed-off-by: Daniel Wagner <wagi@kernel.org>

libnvme/nvme.i
@@ -773,7 +773,7 @@ struct nvme_ns {
 		int ret = 0;
 
 		Py_BEGIN_ALLOW_THREADS  /* Release Python GIL */
-		    ret = nvme_get_log_supported_log_pages(nvme_ctrl_get_fd($self), rae, &log);
+		    ret = nvme_get_log_supported_log_pages(nvme_ctrl_get_link($self), rae, &log);
 		Py_END_ALLOW_THREADS    /* Reacquire Python GIL */
 
 		if (ret < 0) {

linux-nvme/nvme-cli

libnvme/libnvme/nvme.i
@@ -810,7 +810,7 @@ struct nvme_ns {
 		int ret = 0;
 
 		Py_BEGIN_ALLOW_THREADS  /* Release Python GIL */
-		    ret = nvme_get_log_supported_log_pages(nvme_ctrl_get_fd($self), rae, &log);
+		    ret = nvme_get_log_supported_log_pages(nvme_ctrl_get_transport_handle($self), rae, &log);
 		Py_END_ALLOW_THREADS    /* Reacquire Python GIL */
 
 		if (ret < 0) {