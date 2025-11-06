nvme-experiments/libnvme

mi: replace dedicated MI API with generic one
With the introduction of the link handle, it's possible to use the
generic nvme API. The transport selection happens on lower levels
depending on the link object type.

Note, the tests rely on using the ioctl interface and not the io_uring.

Signed-off-by: Daniel Wagner <wagi@kernel.org>

examples/mi-mctp-csi-test.c
@@ -92,7 +92,7 @@ int do_get_log_page(nvme_mi_ep_t ep, int argc, char **argv)
 		return -1;
 	}
 
-	rc = nvme_mi_admin_get_log(link, &args);
+	rc = nvme_get_log(link, &args);
 	if (rc) {
 		warn("can't perform Get Log page command");
 		return -1;

linux-nvme/nvme-cli

libnvme/examples/mi-mctp-csi-test.c
@@ -92,7 +92,7 @@ int do_get_log_page(nvme_mi_ep_t ep, int argc, char **argv)
 		return -1;
 	}
 
-	rc = nvme_mi_admin_get_log(hdl, &args);
+	rc = nvme_get_log(hdl, &args);
 	if (rc) {
 		warn("can't perform Get Log page command");
 		return -1;