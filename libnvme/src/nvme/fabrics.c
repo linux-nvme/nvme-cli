linux-nvme/nvme-cli

commit f4c6eee2939dcf69d9cbc274b53888a28a441bfb

rrc: return error codes directly
The v1 design used the POSIX way to return errors, return -1 with errno
set.

Replace this design by returning directly the error code. That means non
trivial function have to return any data via a pointer so the function
is able to return proper error codes.

Signed-off-by: Daniel Wagner <wagi@kernel.org>

libnvme/src/nvme/fabrics.c
@@ -301,8 +301,7 @@ static int __add_bool_argument(char **argstr, char *tok, bool arg)
 	if (!arg)
 		return 0;
 	if (asprintf(&nstr, "%s,%s", *argstr, tok) < 0) {
-		errno = ENOMEM;
-		return -1;
+		return -ENOMEM;
 	}
 	free(*argstr);
 	*argstr = nstr;
@@ -317,8 +316,7 @@ static int __add_hex_argument(char **argstr, char *tok, int arg, bool allow_zero
 	if (arg < 0 || (!arg && !allow_zero))
 		return 0;
 	if (asprintf(&nstr, "%s,%s=0x%08x", *argstr, tok, arg) < 0) {
-		errno = ENOMEM;
-		return -1;
+		return -ENOMEM;
 	}
 	free(*argstr);
 	*argstr = nstr;
@@ -333,8 +331,7 @@ static int __add_int_argument(char **argstr, char *tok, int arg, bool allow_zero
 	if (arg < 0 || (!arg && !allow_zero))
 		return 0;
 	if (asprintf(&nstr, "%s,%s=%d", *argstr, tok, arg) < 0) {
-		errno = ENOMEM;
-		return -1;
+		return -ENOMEM;
 	}
 	free(*argstr);
 	*argstr = nstr;
@@ -349,8 +346,7 @@ static int __add_int_or_minus_one_argument(char **argstr, char *tok, int arg)
 	if (arg < -1)
 		return 0;
 	if (asprintf(&nstr, "%s,%s=%d", *argstr, tok, arg) < 0) {
-		errno = ENOMEM;
-		return -1;
+		return -ENOMEM;
 	}
 	free(*argstr);
 	*argstr = nstr;
@@ -365,8 +361,7 @@ static int __add_argument(char **argstr, const char *tok, const char *arg)
 	if (!arg || arg[0] == '\0' || !strcmp(arg, "none"))
 		return 0;
 	if (asprintf(&nstr, "%s,%s=%s", *argstr, tok, arg) < 0) {
-		errno = ENOMEM;
-		return -1;
+		return -ENOMEM;
 	}
 	free(*argstr);
 	*argstr = nstr;
@@ -592,23 +587,20 @@ static int build_options(nvme_host_t h, nvme_ctrl_t c, char **argstr)
 
 	if (!transport) {
 		nvme_msg(ctx, LOG_ERR, "need a transport (-t) argument\n");
-		errno = ENVME_CONNECT_TARG;
-		return -1;
+		return -ENVME_CONNECT_TARG;
 	}
 
 	if (strncmp(transport, "loop", 4)) {
 		if (!nvme_ctrl_get_traddr(c)) {
 			nvme_msg(h->ctx, LOG_ERR, "need a address (-a) argument\n");
-			errno = ENVME_CONNECT_AARG;
-			return -1;
+			return -ENVME_CONNECT_AARG;
 		}
 	}
 
 	/* always specify nqn as first arg - this will init the string */
 	if (asprintf(argstr, "nqn=%s",
 		     nvme_ctrl_get_subsysnqn(c)) < 0) {
-		errno = ENOMEM;
-		return -1;
+		return -ENOMEM;
 	}
 
 	if (!strcmp(nvme_ctrl_get_subsysnqn(c), NVME_DISC_SUBSYS_NAME)) {
@@ -630,10 +622,8 @@ static int build_options(nvme_host_t h, nvme_ctrl_t c, char **argstr)
 
 	if (cfg->tls) {
 		ret = __nvme_import_keys_from_config(h, c, &keyring_id, &key_id);
-		if (ret) {
-			errno = -ret;
-			return -1;
-		}
+		if (ret)
+			return ret;
 
 		if (key_id == 0) {
 			if (cfg->tls_configured_key)
@@ -951,8 +941,7 @@ int nvmf_add_ctrl(nvme_host_t h, nvme_ctrl_t c,
 		if (app && strcmp(app, root_app)) {
 			nvme_msg(h->ctx, LOG_INFO, "skip %s, not managed by %s\n",
 				 nvme_subsystem_get_nqn(s), root_app);
-			errno = ENVME_CONNECT_IGNORED;
-			return -1;
+			return -ENVME_CONNECT_IGNORED;
 		}
 	}
 
@@ -960,11 +949,9 @@ int nvmf_add_ctrl(nvme_host_t h, nvme_ctrl_t c,
 	if (traddr_is_hostname(h->ctx, c)) {
 		char *traddr = c->traddr;
 
-		c->traddr = hostname2traddr(h->ctx, traddr);
-		if (!c->traddr) {
+		if (hostname2traddr(h->ctx, traddr, &c->traddr)) {
 			c->traddr = traddr;
-			errno = ENVME_CONNECT_TRADDR;
-			return -1;
+			return -ENVME_CONNECT_TRADDR;
 		}
 		free(traddr);
 	}
@@ -974,10 +961,8 @@ int nvmf_add_ctrl(nvme_host_t h, nvme_ctrl_t c,
 		return ret;
 
 	ret = __nvmf_add_ctrl(h->ctx, argstr);
-	if (ret < 0) {
-		errno = -ret;
-		return -1;
-	}
+	if (ret < 0)
+		return ret;
 
 	nvme_msg(h->ctx, LOG_INFO, "nvme%d: %s connected\n", ret,
 		 nvme_ctrl_get_subsysnqn(c));
@@ -994,18 +979,17 @@ int nvmf_connect_ctrl(nvme_ctrl_t c)
 		return ret;
 
 	ret = __nvmf_add_ctrl(c->s->h->ctx, argstr);
-	if (ret < 0) {
-		errno = -ret;
-		return -1;
-	}
+	if (ret < 0)
+		return ret;
 
 	return 0;
 }
 
-nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
-				    struct nvmf_disc_log_entry *e,
-				    const struct nvme_fabrics_config *cfg,
-				    bool *discover)
+int nvmf_connect_disc_entry(nvme_host_t h,
+			    struct nvmf_disc_log_entry *e,
+			    const struct nvme_fabrics_config *cfg,
+			    bool *discover,
+			    nvme_ctrl_t *cp)
 {
 	const char *transport;
 	char *traddr = NULL, *trsvcid = NULL;
@@ -1025,8 +1009,7 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 			nvme_msg(h->ctx, LOG_ERR,
 				 "skipping unsupported adrfam %d\n",
 				 e->adrfam);
-			errno = EINVAL;
-			return NULL;
+			return -EINVAL;
 		}
 		break;
         case NVMF_TRTYPE_FC:
@@ -1038,8 +1021,7 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 			nvme_msg(h->ctx, LOG_ERR,
 				 "skipping unsupported adrfam %d\n",
 				 e->adrfam);
-			errno = EINVAL;
-			return NULL;
+			return -EINVAL;
 		}
 		break;
 	case NVMF_TRTYPE_LOOP:
@@ -1048,8 +1030,7 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 	default:
 		nvme_msg(h->ctx, LOG_ERR, "skipping unsupported transport %d\n",
 			 e->trtype);
-		errno = EINVAL;
-		return NULL;
+		return -EINVAL;
 	}
 
 	transport = nvmf_trtype_str(e->trtype);
@@ -1057,14 +1038,14 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 	nvme_msg(h->ctx, LOG_DEBUG, "lookup ctrl "
 		 "(transport: %s, traddr: %s, trsvcid %s)\n",
 		 transport, traddr, trsvcid);
-	c = nvme_create_ctrl(h->ctx, e->subnqn, transport, traddr,
-			     cfg->host_traddr, cfg->host_iface, trsvcid);
-	if (!c) {
+
+	ret = nvme_create_ctrl(h->ctx, e->subnqn, transport, traddr,
+			       cfg->host_traddr, cfg->host_iface, trsvcid, &c);
+	if (!ret) {
 		nvme_msg(h->ctx, LOG_DEBUG, "skipping discovery entry, "
 			 "failed to allocate %s controller with traddr %s\n",
 			 transport, traddr);
-		errno = ENOMEM;
-		return NULL;
+		return ret;
 	}
 
 	switch (e->subtype) {
@@ -1092,8 +1073,7 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 
 	if (nvme_ctrl_is_discovered(c)) {
 		nvme_free_ctrl(c);
-		errno = EAGAIN;
-		return NULL;
+		return -EAGAIN;
 	}
 
 	if (e->treq & NVMF_TREQ_DISABLE_SQFLOW &&
@@ -1105,21 +1085,24 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 		c->cfg.tls = true;
 
 	ret = nvmf_add_ctrl(h, c, cfg);
-	if (!ret)
-		return c;
+	if (!ret) {
+		*cp = c;
+		return 0;
+	}
 
-	if (errno == EINVAL && c->cfg.disable_sqflow) {
-		errno = 0;
+	if (ret == EINVAL && c->cfg.disable_sqflow) {
 		/* disable_sqflow is unrecognized option on older kernels */
 		nvme_msg(h->ctx, LOG_INFO, "failed to connect controller, "
 			 "retry with disabling SQ flow control\n");
 		c->cfg.disable_sqflow = false;
 		ret = nvmf_add_ctrl(h, c, cfg);
-		if (!ret)
-			return c;
+		if (!ret) {
+			*cp = c;
+			return 0;
+		}
 	}
 	nvme_free_ctrl(c);
-	return NULL;
+	return -ENOENT;
 }
 
 /*
@@ -1129,8 +1112,8 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
  */
 #define DISCOVERY_HEADER_LEN 20
 
-static struct nvmf_discovery_log *nvme_discovery_log(
-	const struct nvme_get_discovery_args *args)
+static int nvme_discovery_log(const struct nvme_get_discovery_args *args,
+			      struct nvmf_discovery_log **logp)
 {
 	struct nvme_global_ctx *ctx = ctx_from_ctrl(args->c);
 	struct nvmf_discovery_log *log;
@@ -1155,8 +1138,7 @@ static struct nvmf_discovery_log *nvme_discovery_log(
 	if (!log) {
 		nvme_msg(ctx, LOG_ERR,
 			 "could not allocate memory for discovery log header\n");
-		errno = ENOMEM;
-		return NULL;
+		return -ENOMEM;
 	}
 
 	nvme_msg(ctx, LOG_DEBUG, "%s: get header (try %d/%d)\n",
@@ -1184,10 +1166,9 @@ static struct nvmf_discovery_log *nvme_discovery_log(
 		entries_size = sizeof(*log->entries) * numrec;
 		log = __nvme_alloc(sizeof(*log) + entries_size);
 		if (!log) {
-			nvme_msg(ctx, LOG_ERR,
+		nvme_msg(ctx, LOG_ERR,
 				 "could not alloc memory for discovery log page\n");
-			errno = ENOMEM;
-			return NULL;
+			return -ENOMEM;
 		}
 
 		nvme_msg(ctx, LOG_DEBUG,
@@ -1226,22 +1207,21 @@ static struct nvmf_discovery_log *nvme_discovery_log(
 
 	if (genctr != le64_to_cpu(log->genctr)) {
 		nvme_msg(ctx, LOG_INFO, "%s: discover genctr mismatch\n", name);
-		errno = EAGAIN;
+		err = -EAGAIN;
 	} else if (numrec != le64_to_cpu(log->numrec)) {
 		nvme_msg(ctx, LOG_INFO,
 			 "%s: numrec changed unexpectedly "
 			 "from %" PRIu64 " to %" PRIu64 "\n",
 			 name, numrec, le64_to_cpu(log->numrec));
-		errno = EBADSLT;
+		err = -EBADSLT;
 	} else {
-		return log;
+		*logp = log;
+		return 0;
 	}
 
 out_free_log:
 	free(log);
-	if (!errno)
-		errno = nvme_status_to_errno(err, true);
-	return NULL;
+	return err;
 }
 
 static void sanitize_discovery_log_entry(struct nvmf_disc_log_entry *e)
@@ -1260,22 +1240,25 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 		.lsp = NVMF_LOG_DISC_LSP_NONE,
 	};
 
-	*logp = nvmf_get_discovery_wargs(&args);
-	return *logp ? 0 : -1;
+
+	return nvmf_get_discovery_wargs(&args, logp);
 }
 
-struct nvmf_discovery_log *nvmf_get_discovery_wargs(struct nvme_get_discovery_args *args)
+int nvmf_get_discovery_wargs(struct nvme_get_discovery_args *args,
+			     struct nvmf_discovery_log **logp)
 {
 	struct nvmf_discovery_log *log;
+	int err;
 
-	log = nvme_discovery_log(args);
-	if (!log)
-		return NULL;
+	err = nvme_discovery_log(args, &log);
+	if (err)
+		return err;
 
 	for (int i = 0; i < le64_to_cpu(log->numrec); i++)
 		sanitize_discovery_log_entry(&log->entries[i]);
 
-	return log;
+	*logp = log;
+	return 0;
 }
 
 static int uuid_from_device_tree(char *system_uuid)
@@ -1640,40 +1623,35 @@ static int nvmf_dim(nvme_ctrl_t c, enum nvmf_dim_tas tas, __u8 trtype,
 		nvme_msg(ctx, LOG_ERR,
 			 "%s: failed to perform DIM. subsystem undefined.\n",
 			 c->name);
-		errno = EINVAL;
-		return -1;
+		return -EINVAL;
 	}
 
 	if (!c->s->h) {
 		nvme_msg(ctx, LOG_ERR,
 			 "%s: failed to perform DIM. host undefined.\n",
 			 c->name);
-		errno = EINVAL;
-		return -1;
+		return -EINVAL;
 	}
 
 	if (!c->s->h->hostid) {
 		nvme_msg(ctx, LOG_ERR,
 			 "%s: failed to perform DIM. hostid undefined.\n",
 			 c->name);
-		errno = EINVAL;
-		return -1;
+		return -EINVAL;
 	}
 
 	if (!c->s->h->hostnqn) {
 		nvme_msg(ctx, LOG_ERR,
 			 "%s: failed to perform DIM. hostnqn undefined.\n",
 			 c->name);
-		errno = EINVAL;
-		return -1;
+		return -EINVAL;
 	}
 
 	if (strcmp(c->transport, "tcp")) {
 		nvme_msg(ctx, LOG_ERR,
 			 "%s: DIM only supported for TCP connections.\n",
 			 c->name);
-		errno = EINVAL;
-		return -1;
+		return -EINVAL;
 	}
 
 	/* Register one Discovery Information Entry (DIE) of size TEL */
@@ -1682,8 +1660,7 @@ static int nvmf_dim(nvme_ctrl_t c, enum nvmf_dim_tas tas, __u8 trtype,
 
 	dim = (struct nvmf_dim_data *)calloc(1, tdl);
 	if (!dim) {
-		errno = ENOMEM;
-		return -1;
+		return -ENOMEM;
 	}
 
 	dim->tdl    = cpu_to_le32(tdl);
@@ -1698,7 +1675,7 @@ static int nvmf_dim(nvme_ctrl_t c, enum nvmf_dim_tas tas, __u8 trtype,
 	ret = get_entity_name(dim->ename, sizeof(dim->ename));
 	if (ret <= 0)
 		nvme_msg(ctx, LOG_INFO, "%s: Failed to retrieve ENAME. %s.\n",
-			 c->name, strerror(errno));
+			 c->name, strerror(ret));
 
 	ret = get_entity_version(dim->ever, sizeof(dim->ever));
 	if (ret <= 0)
@@ -1767,10 +1744,8 @@ static int nvme_fetch_cntrltype_dctype_from_id(nvme_ctrl_t c)
 	int ret;
 
 	id = __nvme_alloc(sizeof(*id));
-	if (!id) {
-		errno = ENOMEM;
-		return -1;
-	}
+	if (!id)
+		return -ENOMEM;
 
 	ret = nvme_ctrl_identify(c, id);
 	if (ret)
@@ -1803,10 +1778,8 @@ bool nvmf_is_registration_supported(nvme_ctrl_t c)
 
 int nvmf_register_ctrl(nvme_ctrl_t c, enum nvmf_dim_tas tas, __u32 *result)
 {
-	if (!nvmf_is_registration_supported(c)) {
-		errno = ENOTSUP;
-		return -1;
-	}
+	if (!nvmf_is_registration_supported(c))
+		return -ENOTSUP;
 
 	/* We're registering our source address with the DC. To do
 	 * that, we can simply send an empty string. This tells the DC
@@ -1844,7 +1817,7 @@ static char *unescape_uri(const char *str, int len)
 	return dst;
 }
 
-struct nvme_fabrics_uri *nvme_parse_uri(const char *str)
+int nvme_parse_uri(const char *str, struct nvme_fabrics_uri **urip)
 {
 	struct nvme_fabrics_uri *uri;
 	_cleanup_free_ char *scheme = NULL;
@@ -1869,20 +1842,18 @@ struct nvme_fabrics_uri *nvme_parse_uri(const char *str)
 
 	uri = calloc(1, sizeof(struct nvme_fabrics_uri));
 	if (!uri)
-		return NULL;
+		return -ENOMEM;
 
 	if (sscanf(str, "%m[^:/]://%m[^/?#]%ms",
 		   &scheme, &authority, &path) < 2) {
 		nvme_free_uri(uri);
-		errno = EINVAL;
-		return NULL;
+		return -EINVAL;
 	}
 
 	if (sscanf(scheme, "%m[^+]+%ms",
 		   &uri->scheme, &uri->protocol) < 1) {
 		nvme_free_uri(uri);
-		errno = EINVAL;
-		return NULL;
+		return -EINVAL;
 	}
 
 	/* split userinfo */
@@ -1900,8 +1871,7 @@ struct nvme_fabrics_uri *nvme_parse_uri(const char *str)
 		if (sscanf(host, "%m[^:]:%d",
 			   &h, &uri->port) < 1) {
 			nvme_free_uri(uri);
-			errno = EINVAL;
-			return NULL;
+			return -EINVAL;
 		}
 		uri->host = unescape_uri(h, 0);
 	}
@@ -1940,7 +1910,8 @@ struct nvme_fabrics_uri *nvme_parse_uri(const char *str)
 		}
 	}
 
-	return uri;
+	*urip = uri;
+	return 0;
 }
 
 void nvme_free_uri(struct nvme_fabrics_uri *uri)

libnvme/examples/discover-loop.c
@@ -59,22 +59,24 @@ int main()
 
 	nvmf_default_config(&cfg);
 
-	ctx = nvme_scan(NULL);
-	h = nvme_default_host(ctx);
-	if (!h) {
+	ret = nvme_scan(NULL, &ctx);
+	if (ret)
+		return ret;
+	ret = nvme_default_host(ctx, &h);
+	if (ret) {
 		fprintf(stderr, "Failed to allocated memory\n");
-		return ENOMEM;
+		return ret;
 	}
-	c = nvme_create_ctrl(ctx, NVME_DISC_SUBSYS_NAME, "loop",
-			     NULL, NULL, NULL, NULL);
-	if (!c) {
+	ret = nvme_create_ctrl(ctx, NVME_DISC_SUBSYS_NAME, "loop",
+			       NULL, NULL, NULL, NULL, &c);
+	if (ret) {
 		fprintf(stderr, "Failed to allocate memory\n");
 		return ENOMEM;
 	}
 	ret = nvmf_add_ctrl(h, c, &cfg);
-	if (ret < 0) {
+	if (ret) {
 		fprintf(stderr, "no controller found\n");
-		return errno;
+		return ret;
 	}
 
 	ret = nvmf_get_discovery_log(c, &log, 4);

libnvme/examples/display-columnar.c
@@ -24,8 +24,7 @@ int main()
 	nvme_path_t p;
 	nvme_ns_t n;
 
-	ctx = nvme_scan(NULL);
-	if (!ctx)
+	if (nvme_scan(NULL, &ctx))
 		return -1;
 
 	printf("%-16s %-96s %-.16s\n", "Subsystem", "Subsystem-NQN", "Controllers");

libnvme/examples/display-tree.c
@@ -23,8 +23,7 @@ int main()
 	nvme_path_t p, _p;
 	nvme_ns_t n, _n;
 
-	ctx = nvme_scan(NULL);
-	if (!ctx)
+	if (nvme_scan(NULL, &ctx))
 		return -1;
 
 	printf(".\n");

libnvme/examples/mi-mctp-ae.c
@@ -131,7 +131,7 @@ int main(int argc, char **argv)
 	}
 
 	rc = nvme_mi_aem_enable(ep, &aem_config, &data);
-	if (rc && errno == EOPNOTSUPP)
+	if (rc == EOPNOTSUPP)
 		errx(EXIT_FAILURE, "MCTP Peer-Bind is required for AEM");
 	else if (rc)
 		err(EXIT_FAILURE, "Can't enable aem:%d", rc);

libnvme/examples/telemetry-listen.c
@@ -135,8 +135,7 @@ int main()
 	nvme_ctrl_t c;
 	nvme_host_t h;
 
-	ctx = nvme_scan(NULL);
-	if (!ctx)
+	if (nvme_scan(NULL, &ctx))
 		return EXIT_FAILURE;
 
 	nvme_for_each_host(ctx, h)

libnvme/libnvme/nvme.i
@@ -491,7 +491,10 @@ struct nvme_ns {
 
 %extend nvme_global_ctx {
 	nvme_global_ctx(const char *config_file = NULL) {
-		return nvme_scan(config_file);
+		struct nvme_global_ctx *ctx;
+		if (nvme_scan(config_file, &ctx))
+			return NULL;
+		return ctx;
 	}
 	~nvme_global_ctx() {
 		nvme_free_global_ctx($self);
@@ -555,7 +558,11 @@ struct nvme_ns {
 		  const char *hostid = NULL,
 		  const char *hostkey = NULL,
 		  const char *hostsymname = NULL) {
-		nvme_host_t h = hostnqn ? nvme_lookup_host(ctx, hostnqn, hostid) : nvme_default_host(ctx);
+		nvme_host_t h;
+		if (hostnqn)
+			h = nvme_lookup_host(ctx, hostnqn, hostid);
+		if (nvme_default_host(ctx, &h))
+			return NULL;
 		if (hostsymname)
 			nvme_host_set_hostsymname(h, hostsymname);
 		if (hostkey)
@@ -676,8 +683,11 @@ struct nvme_ns {
 		  const char *host_traddr = NULL,
 		  const char *host_iface = NULL,
 		  const char *trsvcid = NULL) {
-		return nvme_create_ctrl(ctx, subsysnqn, transport, traddr,
-					host_traddr, host_iface, trsvcid);
+		struct nvme_ctrl *c;
+		if (nvme_create_ctrl(ctx, subsysnqn, transport, traddr,
+					host_traddr, host_iface, trsvcid, &c))
+			return NULL;
+		return c;
 	}
 	~nvme_ctrl() {
 		nvme_free_ctrl($self);
@@ -780,7 +790,7 @@ struct nvme_ns {
 	%newobject discover;
 	struct nvmf_discovery_log *discover(int lsp = 0, int max_retries = 6) {
 		const char *dev;
-		struct nvmf_discovery_log *logp;
+		struct nvmf_discovery_log *logp = NULL;
 		struct nvme_get_discovery_args args = {
 			.c = $self,
 			.args_size = sizeof(args),
@@ -796,7 +806,7 @@ struct nvme_ns {
 			return NULL;
 		}
 		Py_BEGIN_ALLOW_THREADS  /* Release Python GIL */
-		    logp = nvmf_get_discovery_wargs(&args);
+		    nvmf_get_discovery_wargs(&args, &logp);
 		Py_END_ALLOW_THREADS    /* Reacquire Python GIL */
 
 		if (logp == NULL) discover_err = 2;

libnvme/src/nvme/cleanup.h
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #define __cleanup__(fn) __attribute__((cleanup(fn)))
 
@@ -37,5 +40,8 @@ static inline void cleanup_fd(int *fd)
 		close(*fd);
 }
 #define _cleanup_fd_ __cleanup__(cleanup_fd)
+
+static inline DEFINE_CLEANUP_FUNC(cleanup_addrinfo, struct addrinfo *, freeaddrinfo)
+#define _cleanup_addrinfo_ __cleanup__(cleanup_addrinfo)
 
 #endif

commit ae5b75f72eb9f2068ae856a7a45bf1093d188df2

fabrics: interpret return code from nvme_create_ctrl correctly
When changing how the return code handling is done,
nvmf_connect_disc_entry was not correctly updated. When all is good
there is no error code return.

Fixes: f4c6eee2939d ("rrc: return error codes directly")

Signed-off-by: Daniel Wagner <wagi@kernel.org>

libnvme/src/nvme/fabrics.c
@@ -1041,7 +1041,7 @@ int nvmf_connect_disc_entry(nvme_host_t h,
 
 	ret = nvme_create_ctrl(h->ctx, e->subnqn, transport, traddr,
 			       cfg->host_traddr, cfg->host_iface, trsvcid, &c);
-	if (!ret) {
+	if (ret) {
 		nvme_msg(h->ctx, LOG_DEBUG, "skipping discovery entry, "
 			 "failed to allocate %s controller with traddr %s\n",
 			 transport, traddr);