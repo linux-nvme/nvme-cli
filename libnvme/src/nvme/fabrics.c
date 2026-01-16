62a3ec111d5d1cf9e16a3f65eb97246215fb026a
Martin George <marting@netapp.com>
2026-01-16 12:12:17 +0530

    fabrics: add error if no dhchap-secret is specified with --concat
    --concat requires a corresponding dhchap-secret key to be passed
    with it. So add an appropriate error message if this is not done.
    
    Signed-off-by: Martin George <marting@netapp.com>

--- libnvme/src/nvme/fabrics.c
+++ libnvme/src/nvme/fabrics.c
@@ -737,6 +737,11 @@ static int build_options(nvme_host_t h, nvme_ctrl_t c, char **argstr)
 		return -ENVME_CONNECT_INVAL;
 	}
 
+	if (cfg->concat && !hostkey) {
+		nvme_msg(h->ctx, LOG_ERR, "required argument [--dhchap-secret | -S] not specified with --concat\n");
+		return -ENVME_CONNECT_INVAL;
+	}
+
 	if (cfg->tls) {
 		ret = __nvme_import_keys_from_config(h, c, &keyring_id, &key_id);
 		if (ret)
