7e89b0de2993047d1f74635371de0c496c871e10
Daniel Wagner <wagi@kernel.org>
2026-02-26 18:19:14 +0100

    build: merge update-docs script
    Merge the libnvme update-docs script into the nvme-cli one.
    
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -6,12 +6,30 @@ cd "$(git rev-parse --show-toplevel)" || exit 1
 BUILDDIR="$(mktemp -d)"
 trap 'rm -rf -- $BUILDDIR' EXIT
 
-meson setup                        \
-     -Ddocs=all                    \
-     -Ddocs-build=true             \
-     --force-fallback-for=libnvme  \
-     "${BUILDDIR}"
+meson setup                                 \
+	-Dnvme=disabled                         \
+	-Dlibnvme=disabled                      \
+	-Ddocs=all                              \
+	-Ddocs-build=true                       \
+	"${BUILDDIR}"
 meson compile -C "${BUILDDIR}"
+
+# libnvme
+rm -rf libnvme/doc/man
+mkdir libnvme/doc/man
+find "${BUILDDIR}/libnvme/doc" -maxdepth 1 \
+	-name '*.2' -exec cp {} libnvme/doc/man \;
+
+rm -rf libnvme/doc/rst/*.rst
+mkdir -p libnvme/doc/rst
+find "${BUILDDIR}/libnvme/doc/rst" -maxdepth 1 \
+	-name '*.rst' -exec cp {} libnvme/doc/rst \;
+
+cp "${BUILDDIR}/libnvme/doc/conf.py" libnvme/doc
+cp "${BUILDDIR}/libnvme/doc/index.rst" libnvme/doc
+cp "${BUILDDIR}/libnvme/doc/config-schema.json" libnvme/doc
+
+# nvme-cli
 find "${BUILDDIR}/Documentation" -maxdepth 1 \
      \( -name '*.1' -o -name '*.html' \) \
      -exec cp {} Documentation/ \;
