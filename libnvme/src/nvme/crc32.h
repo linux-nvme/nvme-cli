d61e9a10183c84fbfbda6a7018b1a51351e94440
Daniel Wagner <wagi@kernel.org>
2026-03-03 17:18:29 +0100

    crc32: add spdx license tag
    The crc.[ch] are missing the spdx tag.
    
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- libnvme/src/nvme/crc32.h
+++ libnvme/src/nvme/crc32.h
@@ -1,9 +1,10 @@
-#ifndef crc32_H
-#define crc32_H
+/* SPDX-License-Identifier: LicenseRef-Gary-S-Brown-CRC32 */
+/*
+ * This file is part of libnvme.
+ */
+#pragma once
 
 #include <stdint.h>
 #include <stddef.h>
 
 uint32_t crc32(uint32_t crc, const void *buf, size_t len);
-
-#endif
