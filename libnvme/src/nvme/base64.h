198564ccb48727c64a40bb05967707bfd9709582
Daniel Wagner <wagi@kernel.org>
2026-03-03 17:26:01 +0100

    base64: update spdx tag
    The code is a copy from the nvme-cli project which was licesenced under
    the GPL-2.0-or-later license. The library is LGPL-2.1-or-later. Hannes
    agreed to relicense this code.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- libnvme/src/nvme/base64.h
+++ libnvme/src/nvme/base64.h
@@ -1,8 +1,8 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-#ifndef _BASE64_H
-#define _BASE64_H
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * This file is part of libnvme.
+ */
+#pragma once
 
 int base64_encode(const unsigned char *src, int len, char *dst);
 int base64_decode(const char *src, int len, unsigned char *dst);
-
-#endif /* _BASE64_H */
