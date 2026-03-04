6ec976377ec2bb118aed053216ccd93c651ef24a
Daniel Wagner <wagi@kernel.org>
2026-03-04 14:24:13 +0100

    lib-types: move struct nvme_passthrue_cmd up
    This data structure is used through out the library and there is no
    point in forward declare it and then have it in ioctl.h.
    
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- libnvme/src/nvme/ioctl.h
+++ libnvme/src/nvme/ioctl.h
@@ -9,8 +9,6 @@
 
 #pragma once
 
-#include <linux/types.h>
-
 #include <nvme/lib-types.h>
 
 /**
@@ -32,92 +30,6 @@
  * should not exceed CAP.MQES, 16 is rational for most ssd
  */
 #define NVME_URING_ENTRIES 16
-
-/**
- * struct nvme_passthru_cmd - nvme passthrough command structure
- * @opcode:	Operation code, see &enum nvme_io_opcodes and &enum nvme_admin_opcodes
- * @flags:	Supported only for NVMe-MI
- * @rsvd1:	Reserved for future use
- * @nsid:	Namespace Identifier, or Fabrics type
- * @cdw2:	Command Dword 2 (no spec defined use)
- * @cdw3:	Command Dword 3 (no spec defined use)
- * @metadata:	User space address to metadata buffer (NULL if not used)
- * @addr:	User space address to data buffer (NULL if not used)
- * @metadata_len: Metadata buffer transfer length
- * @data_len:	Data buffer transfer length
- * @cdw10:	Command Dword 10 (command specific)
- * @cdw11:	Command Dword 11 (command specific)
- * @cdw12:	Command Dword 12 (command specific)
- * @cdw13:	Command Dword 13 (command specific)
- * @cdw14:	Command Dword 14 (command specific)
- * @cdw15:	Command Dword 15 (command specific)
- * @timeout_ms:	If non-zero, overrides system default timeout in milliseconds
- * @rsvd2:	Reserved for future use (and fills an implicit struct pad
- * @result:	Set on completion to the command's CQE DWORD 0-1 controller response
- */
-struct nvme_passthru_cmd {
-	__u8    opcode;
-	__u8    flags;
-	__u16   rsvd1;
-	__u32   nsid;
-	__u32   cdw2;
-	__u32   cdw3;
-	__u64   metadata;
-	__u64   addr;
-	__u32   metadata_len;
-	__u32   data_len;
-	__u32   cdw10;
-	__u32   cdw11;
-	__u32   cdw12;
-	__u32   cdw13;
-	__u32   cdw14;
-	__u32   cdw15;
-	__u32   timeout_ms;
-	__u32   rsvd2;
-	__u64   result;
-};
-
-/**
- * struct nvme_uring_cmd - nvme uring command structure
- * @opcode:	Operation code, see &enum nvme_io_opcodes and &enum nvme_admin_opcodes
- * @flags:	Not supported: intended for command flags (eg: SGL, FUSE)
- * @rsvd1:	Reserved for future use
- * @nsid:	Namespace Identifier, or Fabrics type
- * @cdw2:	Command Dword 2 (no spec defined use)
- * @cdw3:	Command Dword 3 (no spec defined use)
- * @metadata:	User space address to metadata buffer (NULL if not used)
- * @addr:	User space address to data buffer (NULL if not used)
- * @metadata_len: Metadata buffer transfer length
- * @data_len:	Data buffer transfer length
- * @cdw10:	Command Dword 10 (command specific)
- * @cdw11:	Command Dword 11 (command specific)
- * @cdw12:	Command Dword 12 (command specific)
- * @cdw13:	Command Dword 13 (command specific)
- * @cdw14:	Command Dword 14 (command specific)
- * @cdw15:	Command Dword 15 (command specific)
- * @timeout_ms:	If non-zero, overrides system default timeout in milliseconds
- * @rsvd2:	Reserved for future use (and fills an implicit struct pad
- */
-struct nvme_uring_cmd {
-	__u8	opcode;
-	__u8	flags;
-	__u16	rsvd1;
-	__u32	nsid;
-	__u32	cdw2;
-	__u32	cdw3;
-	__u64	metadata;
-	__u64	addr;
-	__u32	metadata_len;
-	__u32	data_len;
-	__u32	cdw10;
-	__u32	cdw11;
-	__u32	cdw12;
-	__u32	cdw13;
-	__u32	cdw14;
-	__u32	cdw15;
-	__u32	timeout_ms;
-	__u32   rsvd2;
-};
 
 /**
  * nvme_submit_admin_passthru() - Submit an nvme passthrough admin command
