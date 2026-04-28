c3fa82e0e0223839a6f5c6cbc3d5956abb058784
Nilay Shroff <nilay@linux.ibm.com>
2026-04-28 20:02:40 +0530

    nvme: add sigaction for SIGWINCH
    Add a sigaction handler for SIGWINCH so that nvme-top can detect
    terminal window size changes.
    
    This allows the dashboard layout to be adjusted and redrawn when the
    terminal is resized. To avoid undefined behavior and async-signal-safety
    the data type for nvme_sigwinch_received is defined as volatile
    sig_atomic_t.
    
    While we are at it, also update the data type of nvme_sigint_received
    to volatile sig_atomic_t.
    
    Signed-off-by: Nilay Shroff <nilay@linux.ibm.com>

--- util/sighdl.c
+++ util/sighdl.c
@@ -5,16 +5,22 @@
 
 #include "sighdl.h"
 
-bool nvme_sigint_received;
+volatile sig_atomic_t nvme_sigint_received;
+volatile sig_atomic_t nvme_sigwinch_received;
 
 static void nvme_sigint_handler(int signum)
 {
 	nvme_sigint_received = true;
 }
 
+static void nvme_sigwinch_handler(int signum)
+{
+	nvme_sigwinch_received = true;
+}
+
 int nvme_install_sigint_handler(void)
 {
-	struct sigaction act;
+	struct sigaction act = {0};
 
 	sigemptyset(&act.sa_mask);
 	act.sa_handler = nvme_sigint_handler;
@@ -22,6 +28,21 @@ int nvme_install_sigint_handler(void)
 
 	nvme_sigint_received = false;
 	if (sigaction(SIGINT, &act, NULL) == -1)
+		return -errno;
+
+	return 0;
+}
+
+int nvme_install_sigwinch_handler(void)
+{
+	struct sigaction act = {0};
+
+	sigemptyset(&act.sa_mask);
+	act.sa_handler = nvme_sigwinch_handler;
+	act.sa_flags = 0;
+
+	nvme_sigwinch_received = false;
+	if (sigaction(SIGWINCH, &act, NULL) == -1)
 		return -errno;
 
 	return 0;
--- util/sighdl.h
+++ util/sighdl.h
@@ -3,9 +3,12 @@
 #define __NVME_SIGHDL
 
 #include <stdbool.h>
+#include <signal.h>
 
-extern bool nvme_sigint_received;
+extern volatile sig_atomic_t nvme_sigint_received;
+extern volatile sig_atomic_t nvme_sigwinch_received;
 
 int nvme_install_sigint_handler(void);
+int nvme_install_sigwinch_handler(void);
 
 #endif // __NVME_SIGHDL
