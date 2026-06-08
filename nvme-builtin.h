01a392cb0d2c529297c67406e4a95557b0c03af3
Broc Going <bgoing@micron.com>
2026-06-08 09:10:51 -0700

    top: Make top a conditional feature and disable by default on Windows
    Adds a 'top' configuration option that is enabled by default, and makes
    compilation of nvme-top code conditional on the feature enablement.
    Disables the top feature by default on Windows due to compatibility
    issues.
    
    Signed-off-by: Broc Going <bgoing@micron.com>

--- nvme-builtin.h
+++ nvme-builtin.h
@@ -103,7 +103,9 @@ COMMAND_LIST(
 	ENTRY("show-regs", "Shows the controller registers or properties. Requires character device", show_registers)
 	ENTRY("set-reg", "Set a register and show the resulting value", set_register)
 	ENTRY("get-reg", "Get a register and show the resulting value", get_register)
+#ifdef CONFIG_TOP
 	ENTRY("top", "nvme top", top)
+#endif
 #ifdef CONFIG_FABRICS
 	ENTRY("discover", "Discover NVMeoF subsystems", discover_cmd)
 	ENTRY("connect-all", "Discover and Connect to NVMeoF subsystems", connect_all_cmd)
