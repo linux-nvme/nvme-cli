f66408f5e0f2106e7ab4db6160426273a9a7217c
Brandon Capener <bcapener@micron.com>
2026-06-18 13:17:39 -0700

    lib: add windows direct open transport handle support
    rename __nvme_transport_handle_open_direct to
    __libnvme_transport_handle_open_direct.
    Implement functionality for the windows
    __libnvme_transport_handle_open_direct function.
    
    Signed-off-by: Brandon Capener <bcapener@micron.com>

--- libnvme/src/nvme/lib-win.c
+++ libnvme/src/nvme/lib-win.c
@@ -10,9 +10,83 @@
 #include <stdio.h>
 #include <strings.h>
 
+#include "cleanup.h"
 #include "compiler-attributes.h"
 #include "private.h"
 
+static bool __is_controller_path(const char *device_path)
+{
+	/*
+	 * Controller device paths point to the PCI device, and begin with
+	 * "\\?\pci". Namespace device paths point to the disk device and begin
+	 * "\\.\PhysicalDrive".
+	 */
+	return strncasecmp(device_path, "\\\\?\\pci", 7) == 0;
+}
+
+static __libnvme_unused int __libnvme_transport_handle_open_direct(
+	struct libnvme_transport_handle *hdl, const char *name)
+{
+	__cleanup_free char *device_path = NULL;
+
+	/* Parse and open direct device */
+	hdl->type = LIBNVME_TRANSPORT_HANDLE_TYPE_DIRECT;
+
+	/* Convert device name to Windows path */
+	if (strncmp(name, "\\\\.\\", 4) == 0
+	    || strncmp(name, "\\\\?\\", 4) == 0) {
+		/* Already a Windows device path */
+		device_path = strdup(name);
+		if (!device_path)
+			return -ENOMEM;
+	} else {
+		/* Assume it's a device name, prepend Windows device prefix */
+		/* PhysicalDriveN format */
+		size_t len = strlen(name) + 5;
+		int ret;
+
+		device_path = malloc(len);
+		if (!device_path)
+			return -ENOMEM;
+		ret = snprintf(device_path, len, "\\\\.\\%s", name);
+		if (ret < 0 || (size_t)ret >= len)
+			return -EINVAL;
+	}
+
+	hdl->fd = CreateFileA(device_path,
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+		NULL,
+		OPEN_EXISTING,
+		0,
+		NULL);
+
+	if (hdl->fd == INVALID_HANDLE_VALUE) {
+		int err = GetLastError();
+		/* Map Windows error to errno */
+		switch (err) {
+		case ERROR_FILE_NOT_FOUND:
+		case ERROR_PATH_NOT_FOUND:
+			return -ENOENT;
+		case ERROR_ACCESS_DENIED:
+			return -EACCES;
+		default:
+			return -EIO;
+		}
+	}
+
+	/* Mark st_mode based on device type for compatibility with Linux. */
+	memset(&hdl->stat, 0, sizeof(hdl->stat));
+	hdl->stat.st_mode =
+		(__is_controller_path(device_path) ? S_IFCHR : S_IFBLK) | 0600;
+	hdl->stat.st_nlink = 1;
+
+	/* Windows doesn't distinguish 32/64-bit ioctl, always 64-bit capable */
+	hdl->ioctl_admin_state = IOCTL_STATE_IOCTL64;
+	hdl->ioctl_io_state = IOCTL_STATE_IOCTL64;
+
+	return 0;
+}
 
 __libnvme_public int libnvme_open(struct libnvme_global_ctx *ctx,
 				  const char *name,
