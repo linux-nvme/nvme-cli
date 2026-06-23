8fbda1532fd5164870ee3e45e2b311bbd9ecf8ff
Broc Going <bgoing@micron.com>
2026-06-23 11:49:22 -0700

    micron: Add cross-platform support for getting drive model
    - Adds micron-utils with separate Linux and Windows implementations.
    - Adds utility method for getting PCIe ID values which are used to
    determine drive model.
    - Removes hard-coded sysfs paths in favor of using libnvme utils to get
    sysfs paths.
    
    Signed-off-by: Broc Going <bgoing@micron.com>

---
+++ plugins/micron/micron-utils-win.c
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+ * Copyright (c) 2025 Micron Technology, Inc.
+ *
+ * Authors: Broc Going <bgoing@micron.com>
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <windows.h>
+
+#include <libnvme.h>
+
+#include "micron-utils.h"
+#include "util/cleanup.h"
+
+int micron_get_pci_ids(struct libnvme_global_ctx *ctx,
+			struct libnvme_transport_handle *hdl,
+			unsigned short *vid, unsigned short *did)
+{
+	const char *p;
+	unsigned int val;
+
+	/* Windows sysfs dir for controller contains VID and DID */
+	__cleanup_free char *ctrl_sysfs_dir = micron_get_ctrl_sysfs_dir(ctx, hdl);
+
+	*vid = 0;
+	*did = 0;
+
+	if (!ctrl_sysfs_dir)
+		return -EINVAL;
+
+	p = strstr(ctrl_sysfs_dir, "ven_");
+	if (p && sscanf(p, "ven_%x", &val) == 1)
+		*vid = (unsigned short)val;
+	else
+		return -EINVAL;
+
+	p = strstr(ctrl_sysfs_dir, "dev_");
+	if (p && sscanf(p, "dev_%x", &val) == 1)
+		*did = (unsigned short)val;
+	else
+		return -EINVAL;
+
+	return 0;
+}
