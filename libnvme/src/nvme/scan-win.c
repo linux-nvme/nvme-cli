67c6e212cb50a444ea45382e962ed7b9021ff0f1
Brandon Capener <bcapener@micron.com>
2026-06-24 11:29:00 -0700

    tree: add windows tree support
    Add tree.c and lib.c support. Adds ctrl-map.c for mapping controller name,
    nvmeX, to controller path, since windows controller paths might not fit
    within the d_name field of the dirent struct.
    The ctrl-map also creates a subsystem name to replicate
    linux's nvme-subsysX naming.
    
    Signed-off-by: Brandon Capener <bcapener@micron.com>

--- libnvme/src/nvme/scan-win.c
+++ libnvme/src/nvme/scan-win.c
@@ -12,6 +12,7 @@
 #include <libnvme.h>
 
 #include "private.h"
+#include "private-ctrl-map.h"
 #include "compiler-attributes.h"
 
 __libnvme_public int libnvme_scan_subsystems(
@@ -32,7 +33,48 @@ __libnvme_public int libnvme_scan_ctrls(
 	__libnvme_unused struct libnvme_global_ctx *ctx,
 	__libnvme_unused struct dirent ***ctrls)
 {
-	return -ENOTSUP;
+	struct dirent **entries;
+	size_t i, count;
+	int ret;
+
+	if (!ctrls)
+		return -EINVAL;
+
+	*ctrls = NULL;
+
+	libnvme_ctrl_map_clear();
+	ret = libnvme_ctrl_map_init();
+	if (ret)
+		return ret;
+
+	count = libnvme_ctrl_map_get_count();
+	if (!count)
+		return 0;
+
+	entries = calloc(count, sizeof(*entries));
+	if (!entries)
+		goto enomem;
+
+	for (i = 0; i < count; i++) {
+		entries[i] = calloc(1, sizeof(*entries[i]));
+		if (!entries[i])
+			goto enomem;
+		snprintf(entries[i]->d_name,
+			 sizeof(entries[i]->d_name), "%s",
+			 libnvme_ctrl_map_get_name(i));
+	}
+
+	*ctrls = entries;
+	return (int)count;
+
+enomem:
+	libnvme_ctrl_map_clear();
+	if (entries) {
+		while (i > 0)
+			free(entries[--i]);
+		free(entries);
+	}
+	return -ENOMEM;
 }
 
 __libnvme_public int libnvme_scan_ctrl_namespace_paths(
@@ -46,7 +88,58 @@ __libnvme_public int libnvme_scan_ctrl_namespaces(
 	__libnvme_unused libnvme_ctrl_t c,
 	__libnvme_unused struct dirent ***ns)
 {
-	return -ENOTSUP;
+	struct dirent **entries = NULL;
+	const struct ctrl_map_entry *ctrl_entry;
+	DWORD *device_numbers = NULL;
+	int dev_count = 0;
+	int ret;
+	int i;
+
+	if (!c || !ns)
+		return -EINVAL;
+
+	*ns = NULL;
+
+	ctrl_entry = libnvme_ctrl_map_lookup(c->name);
+	if (!ctrl_entry)
+		return 0;
+
+	ret = libnvme_ctrl_map_entry_scan_device_numbers(ctrl_entry,
+							 &device_numbers,
+							 &dev_count);
+	if (ret)
+		return ret;
+
+	if (!dev_count)
+		return 0;
+
+	entries = calloc(dev_count, sizeof(*entries));
+	if (!entries) {
+		free(device_numbers);
+		return -ENOMEM;
+	}
+
+	for (i = 0; i < dev_count; i++) {
+		entries[i] = calloc(1, sizeof(*entries[i]));
+		if (!entries[i])
+			goto enomem;
+
+		snprintf(entries[i]->d_name,
+			 sizeof(entries[i]->d_name),
+			 "\\\\.\\PhysicalDrive%lu",
+			 device_numbers[i]);
+	}
+
+	free(device_numbers);
+	*ns = entries;
+	return dev_count;
+
+enomem:
+	while (i > 0)
+		free(entries[--i]);
+	free(entries);
+	free(device_numbers);
+	return -ENOMEM;
 }
 
 __libnvme_public int libnvme_scan_ns_head_paths(
