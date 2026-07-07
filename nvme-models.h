a0bdaa2a3e47af1f73198d3b720bd897aaf414df
Broc Going <bgoing@micron.com>
2026-07-07 07:25:14 -0700

    nvme-models: Update product name detection to use nvme-pci-ids
    Refactors nvme-models to use the new, cross-platform pci ID detection
    support in nvme-pci-ids.  The nvme_product_name method is now platform
    independent, and will work on Windows if a pci.ids file is specified in
    the PCI_IDS_PATH environment variable.
    
    Signed-off-by: Broc Going <bgoing@micron.com>

--- nvme-models.h
+++ nvme-models.h
@@ -1,4 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
-char *nvme_product_name(const char *devname);
+char *nvme_product_name(struct libnvme_global_ctx *ctx,
+		struct libnvme_transport_handle *hdl);
