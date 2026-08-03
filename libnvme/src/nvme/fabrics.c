60060bd06833e81aa4120cfcc48e823f29bc5fb1
Daniel Wagner <wagi@kernel.org>
2026-08-03 15:58:11 +0200

    libnvme/fabrics: set kato before discovery starts
    dc_should_connect should not have any side effects. Move the KATO
    update to just before connecting to the next Discovery Controller.
    
    nfctx first copies the default value from fctx, and
    set_discovery_kato() only updates nfctx. Therefore there is no need to
    set the value directly. In fact, doing so undoes the value set by
    set_discovery_kato().
    
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- libnvme/src/nvme/fabrics.c
+++ libnvme/src/nvme/fabrics.c
@@ -2617,8 +2617,6 @@ static bool dc_should_connect(struct libnvme_global_ctx *ctx,
 			else
 				disconnect = false;
 		}
-
-		set_discovery_kato(fctx);
 	} else {
 		/* NVME_NQN_NVME */
 		disconnect = false;
@@ -2661,7 +2659,6 @@ static int _nvmf_discovery(struct libnvme_global_ctx *ctx,
 		bool discover = false;
 		bool disconnect;
 		libnvme_ctrl_t child = { 0 };
-		int tmo = fctx->ctrl_params.cfg.keep_alive_tmo;
 		struct libnvmf_context nfctx = *fctx;
 
 		sanitize_discovery_log_entry(c->ctx, e);
@@ -2686,11 +2683,11 @@ static int _nvmf_discovery(struct libnvme_global_ctx *ctx,
 
 		err = nvmf_connect_disc_entry(h, e, &nfctx, &discover, &child);
 
-		nfctx.ctrl_params.cfg.keep_alive_tmo = tmo;
-
 		if (child) {
-			if (discover)
+			if (discover) {
+				set_discovery_kato(&nfctx);
 				_nvmf_discovery(ctx, &nfctx, true, child);
+			}
 
 			if (disconnect) {
 				libnvmf_disconnect_ctrl(child);
