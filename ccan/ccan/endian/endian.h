beb53a1b54f4f43e2e373069188394e637260847
Daniel Wagner <dwagner@suse.com>
2026-08-05 19:28:05 +0200

    ccan/endian: guard endian defintion
    FreeBSD defines those defines thus add guards.
    
    Signed-off-by: Daniel Wagner <dwagner@suse.com>

--- ccan/ccan/endian/endian.h
+++ ccan/ccan/endian/endian.h
@@ -103,8 +103,12 @@ static inline uint64_t bswap_64(uint64_t val)
 #endif
 
 /* Needed for Glibc like endiness check */
+#ifndef __LITTLE_ENDIAN
 #define	__LITTLE_ENDIAN	1234
+#endif
+#ifndef __BIG_ENDIAN
 #define	__BIG_ENDIAN	4321
+#endif
 
 /* Sanity check the defines.  We don't handle weird endianness. */
 #if !HAVE_LITTLE_ENDIAN && !HAVE_BIG_ENDIAN
