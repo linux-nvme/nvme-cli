87b6661f2187c2175bdf7e8611d4292f29ac2667
Tokunori Ikegami <ikegami.t@gmail.com>
2026-08-09 08:24:10 +0900

    nvme-types-base: add spec v2.4 feature IDs
    To update libnvme APIs to spec v2.4.
    
    Signed-off-by: Tokunori Ikegami <ikegami.t@gmail.com>

--- src/nvme-print.c
+++ src/nvme-print.c
@@ -1030,6 +1030,9 @@ const char *nvme_feature_to_string(enum nvme_features_id feature)
 	case NVME_FEAT_FID_POWER_LIMIT:		return "Power Limit";
 	case NVME_FEAT_FID_POWER_THRESH:	return "Power Threshold";
 	case NVME_FEAT_FID_POWER_MEASUREMENT:	return "Power Measurement";
+	case NVME_FEAT_FID_VOLTAGE_THRESHOLD:	return "Voltage Threshold";
+	case NVME_FEAT_FID_VOLTAGE_MEASUREMENT:	return "Voltage Measurement";
+	case NVME_FEAT_FID_RATE_LIMITING:	return "Rate Limiting";
 	case NVME_FEAT_FID_EMB_MGMT_CTRL_ADDR:	return "Embedded Management Controller Address";
 	case NVME_FEAT_FID_HOST_MGMT_AGENT_ADDR:return "Host Management Agent Address";
 	case NVME_FEAT_FID_ENH_CTRL_METADATA:	return "Enhanced Controller Metadata";
