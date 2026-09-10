234e2a640e174ac0e9f0b2977693a4494ecbd0d9
Daniel Wagner <wagi@monom.org>
2026-09-10 12:09:52 +0000

    ocp: guard reason-identifier parse against NULL ptelemetry_buffer
    print_ocp_telemetry_normal() already treats a NULL ptelemetry_buffer
    as "nothing to parse" for the host/controller log-page header block,
    but the very next block unconditionally computed
    `ptelemetry_buffer + offsetof(...)` and handed it to
    generic_structure_parser(), in both the file-output and stdout-output
    copies of this code. Pointer arithmetic on a null base is undefined
    behaviour, and generic_structure_parser() would then dereference near
    address zero.
    
    Extend the existing NULL check to cover the reason-identifier block
    too, consistent with the header block right above it.
    
    Signed-off-by: Daniel Wagner <wagi@monom.org>

--- plugins/ocp/ocp-telemetry-decode.c
+++ plugins/ocp/ocp-telemetry-decode.c
@@ -1690,12 +1690,16 @@ int print_ocp_telemetry_normal(struct ocp_telemetry_parse_options *options)
 			fprintf(fp, STR_LINE);
 			fprintf(fp, "%s\n", STR_REASON_IDENTIFIER);
 			fprintf(fp, STR_LINE);
-			__u8 *preason_identifier_offset = ptelemetry_buffer +
-				offsetof(struct nvme_ocp_telemetry_host_initiated_header,
-				reason_id);
+			if (ptelemetry_buffer == NULL) {
+				printf("skip generic_structure_parser\n");
+			} else {
+				__u8 *preason_identifier_offset = ptelemetry_buffer +
+					offsetof(struct nvme_ocp_telemetry_host_initiated_header,
+					reason_id);
 
-			generic_structure_parser(preason_identifier_offset, reason_identifier,
-				ARRAY_SIZE(reason_identifier), NULL, 0, fp);
+				generic_structure_parser(preason_identifier_offset, reason_identifier,
+					ARRAY_SIZE(reason_identifier), NULL, 0, fp);
+			}
 
 			fprintf(fp, STR_LINE);
 			fprintf(fp, "%s\n", STR_TELEMETRY_HOST_DATA_BLOCK_1);
@@ -1807,10 +1811,14 @@ int print_ocp_telemetry_normal(struct ocp_telemetry_parse_options *options)
 		printf(STR_LINE);
 		printf("%s\n", STR_REASON_IDENTIFIER);
 		printf(STR_LINE);
-		__u8 *preason_identifier_offset = ptelemetry_buffer +
-			offsetof(struct nvme_ocp_telemetry_host_initiated_header, reason_id);
-		generic_structure_parser(preason_identifier_offset, reason_identifier,
-			ARRAY_SIZE(reason_identifier), NULL, 0, NULL);
+		if (ptelemetry_buffer == NULL) {
+			printf("skip generic_structure_parser\n");
+		} else {
+			__u8 *preason_identifier_offset = ptelemetry_buffer +
+				offsetof(struct nvme_ocp_telemetry_host_initiated_header, reason_id);
+			generic_structure_parser(preason_identifier_offset, reason_identifier,
+				ARRAY_SIZE(reason_identifier), NULL, 0, NULL);
+		}
 
 		printf(STR_LINE);
 		printf("%s\n", STR_TELEMETRY_HOST_DATA_BLOCK_1);
