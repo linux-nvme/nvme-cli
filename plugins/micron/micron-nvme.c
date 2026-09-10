0c1516363d7e2d7b9522c6e32ea38377e4f28de7
Daniel Wagner <wagi@monom.org>
2026-09-10 12:31:16 +0000

    micron: don't let closedir() clobber errno in RemoveDirRecursive()
    Three of this function's error paths compute the errno to report
    (from unlink()/a recursive call, or ENAMETOOLONG) and then call
    closedir() before returning -1 -- but closedir() isn't guaranteed to
    leave errno alone, so it could overwrite the very value the caller is
    about to read. Save it across the closedir() call.
    
    Signed-off-by: Daniel Wagner <wagi@monom.org>

--- plugins/micron/micron-nvme.c
+++ plugins/micron/micron-nvme.c
@@ -305,8 +305,8 @@ static int RemoveDirRecursive(const char *path)
 
 		if (snprintf(child, sizeof(child), "%s/%s", path, entry->d_name) >=
 		    (int)sizeof(child)) {
-			errno = ENAMETOOLONG;
 			closedir(dir);
+			errno = ENAMETOOLONG;
 			return -1;
 		}
 
@@ -319,14 +319,20 @@ static int RemoveDirRecursive(const char *path)
 		 * to attempt recursive removal.
 		 */
 		if (errno != EISDIR && errno != EPERM && errno != EACCES) {
+			int saved_errno = errno;
+
 			closedir(dir);
+			errno = saved_errno;
 			return -1;
 		}
 
 		if (RemoveDirRecursive(child) < 0) {
-			if (errno == ENOENT)
+			int saved_errno = errno;
+
+			if (saved_errno == ENOENT)
 				continue;
 			closedir(dir);
+			errno = saved_errno;
 			return -1;
 		}
 	}
