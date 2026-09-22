76d80af8f18811c655ed24f3e099b9ca35af1b10
Daniel Wagner <wagi@monom.org>
2026-09-22 17:06:56 +0000

    nvme-print-stdout: convert stdout_fdp_usage() to shr_table
    No prior alignment bug (bare "name: value" lines); converted for
    consistency. Also replaced the nested-ternary RUHA-to-string lookup
    with a plain switch for readability while touching the line anyway.
    
    Signed-off-by: Daniel Wagner <wagi@monom.org>

--- src/nvme-print-stdout.c
+++ src/nvme-print-stdout.c
@@ -1299,15 +1299,42 @@ static void stdout_fdp_configs(struct nvme_fdp_config_log *log, size_t len)
 static void stdout_fdp_usage(struct nvme_fdp_ruhu_log *log, size_t len)
 {
 	uint16_t nruh = le16_to_cpu(log->nruh);
+	struct shr_table *t;
+
+	t = stdout_kv_table_create();
+	if (!t)
+		return;
 
 	for (int i = 0; i < nruh; i++) {
 		struct nvme_fdp_ruhu_desc *ruhu = &log->ruhus[i];
+		const char *ruha_str;
+		char name[40];
 
-		printf("Reclaim Unit Handle %d Attributes: %#"PRIx8" (%s)\n", i, ruhu->ruha,
-				ruhu->ruha == 0x0 ? "Unused" : (
-				ruhu->ruha == 0x1 ? "Host Specified" : (
-				ruhu->ruha == 0x2 ? "Controller Specified" : "Unknown")));
+		switch (ruhu->ruha) {
+		case 0x0:
+			ruha_str = "Unused";
+			break;
+		case 0x1:
+			ruha_str = "Host Specified";
+			break;
+		case 0x2:
+			ruha_str = "Controller Specified";
+			break;
+		default:
+			ruha_str = "Unknown";
+			break;
+		}
+
+		snprintf(name, sizeof(name),
+			 "Reclaim Unit Handle %d Attributes", i);
+		stdout_kv_add(t, name, "%#"PRIx8" (%s)", ruhu->ruha, ruha_str);
 	}
+
+	if (shr_table_has_error(t))
+		fprintf(stderr, "Failed to build fdp-usage table\n");
+	else
+		stdout_kv_render(stdout, t);
+	shr_table_free(t);
 }
 
 static void stdout_fdp_stats(struct nvme_fdp_stats_log *log)
